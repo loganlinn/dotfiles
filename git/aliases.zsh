2e001c0557c455812d18480ad4734df9446356ad
loganlinn <logan.linn@gmail.com>
2011-09-28 10:03:36 -0700

    Add wrapper alias for hub

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,14 +7,15 @@
 #
 # I'm hardcoding it to an installed rvm (using rvm's `rvm 1.8.7,ruby /hub/path`
 # syntax is way too slow). It should work fine for those without rvm, though.
-if [[ -s $HOME/.rvm/scripts/rvm ]]
-then
-  if $(which hub &> /dev/null) && [[ -s $HOME/.rvm/rubies/ruby-1.8.7-p334 ]]
-  then
-    alias git='$HOME/.rvm/rubies/ruby-1.8.7-p334/bin/ruby `which hub`'
-  else
-  fi
-fi
+# if [[ -s $HOME/.rvm/scripts/rvm ]]
+# then
+#   if $(which hub &> /dev/null) && [[ -s $HOME/.rvm/rubies/ruby-1.8.7-p334 ]]
+#   then
+#     alias git='$HOME/.rvm/rubies/ruby-1.8.7-p334/bin/ruby `which hub`'
+#   else
+#   fi
+# fi
+alias git=hub
 
 # The rest of my fun git aliases
 alias gl='git pull --prune'
