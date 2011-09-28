7123cb731fc6611786f29e75e5116cf072025716
loganlinn <logan.linn@gmail.com>
2011-09-28 10:30:58 -0700

    Add git pull rebase alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -19,6 +19,7 @@ alias git=hub
 
 # The rest of my fun git aliases
 alias gl='git pull --prune'
+alias glr='git pull --rebase'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
