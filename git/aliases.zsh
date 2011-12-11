d393a37ca6f1932d4e0abdfc8c10a20141bb2911
loganlinn <logan.linn@gmail.com>
2011-12-11 16:20:23 -0500

    Update git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -20,8 +20,9 @@ alias git=hub
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glr='git pull --rebase'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias glg="git log --graph --pretty=format:'%Cred%h%Creset %Cblue%an%Creset: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
+alias gph='git push heroku HEAD'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
