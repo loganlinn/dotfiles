07e015e2aaac5a2faa31a1af170ec1a98a8e4c15
loganlinn <logan.linn@gmail.com>
2012-02-26 16:20:29 -0800

    Update git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -19,6 +19,7 @@ alias git=hub
 
 # The rest of my fun git aliases
 alias gl='git pull --prune'
+alias glo='git pull origin'
 alias glr='git pull --rebase'
 alias glg="git log --graph --pretty=format:'%Cred%h%Creset %Cblue%an%Creset: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
@@ -26,8 +27,10 @@ alias gph='git push heroku HEAD'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
+alias gcm='git commit --amend'
 alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 alias gdt='git dt'
+alias gstash='git stash -u'
