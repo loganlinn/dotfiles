34209b99639aec21d839f19b33f33a93e25c4f2b
loganlinn <logan.linn@gmail.com>
2012-04-18 15:14:04 -0700

    Add explict common readline shortucts
    These were working while in a tmux session

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -46,3 +46,7 @@ bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char
 bindkey '^[^N' newtab
 bindkey '^?' backward-delete-char
+
+bindkey '^a' beginning-of-line
+bindkey '^e' end-of-line
+bindkey '^r' history-incremental-search-backward
