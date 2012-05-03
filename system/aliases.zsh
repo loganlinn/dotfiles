f700608f90a884bace48356e6dd8a364fb2d2b3c
loganlinn <logan.linn@gmail.com>
2012-05-03 16:59:27 -0700

    Remove $() from gls

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,7 +1,7 @@
 # grc overides for ls
 #   Made possible through contributions from generous benefactors like
 #   `brew install coreutils`
-if $(gls &>/dev/null)
+if which gls &>/dev/null
 then
   alias ls="gls -F --color"
   alias l="gls -lAh --color"
