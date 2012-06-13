7f91d895b2374b4e5bc3b3119b9019a2c1e012a9
loganlinn <logan.linn@gmail.com>
2012-06-13 09:11:58 -0700

    Fix glrp alias assignment syntax

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -12,7 +12,7 @@ alias glo='gl && git pull origin'
 alias glr='git pull --rebase'
 alias glg="git log --graph --pretty=format:'%Cred%h%Creset %Cblue%an%Creset: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
-alias glrp = 'glr && gp'
+alias glrp='glr && gp'
 alias gph='git push heroku HEAD'
 alias gd='git diff'
 alias gc='git commit'
