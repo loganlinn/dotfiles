3fffd5adca6c95482a92942c5505ff0d75d303e3
loganlinn <logan.linn@gmail.com>
2012-08-22 16:00:20 -0700

    Export VIMCLOJURE_SERVER_JAR
    When it exists in the standard location

--- system/env.zsh
+++ system/env.zsh
@@ -2,5 +2,9 @@
 export EDITOR='vim'
 
 if [[ $(uname -s) == "Linux" && "$TERM" == "xterm" ]]; then
-  export TERM='xterm-256color'
+	export TERM='xterm-256color'
+fi
+
+if [ -e $HOME/vimclojure/server/server.jar ]; then
+	export VIMCLOJURE_SERVER_JAR=$HOME/vimclojure/server/server.jar
 fi
