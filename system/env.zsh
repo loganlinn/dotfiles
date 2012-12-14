0549469dfb919d1c2bd3e5fb103c1369e047c866
loganlinn <logan.linn@gmail.com>
2012-12-14 10:31:03 -0500

    Add HUDNS env variable

--- system/env.zsh
+++ system/env.zsh
@@ -8,3 +8,7 @@ fi
 if [ -e $HOME/vimclojure/server/server.jar ]; then
 	export VIMCLOJURE_SERVER_JAR=$HOME/vimclojure/server/server.jar
 fi
+
+if [ -e $PROJECTS/huddler ]; then
+  export HUDNS=$PROJECTS/huddler/v2/system/application/libraries/Huddler
+fi
