88d832a27d8901862085c26b52cbec8c6e4bde51
loganlinn <logan.linn@gmail.com>
2012-12-17 23:32:36 -0500

    set-upstream by defaltttt
    This is what I've wanted all along :(

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -11,7 +11,7 @@ alias gl='git pull --prune'
 alias glo='gl && git pull origin'
 alias glr='git pull --rebase'
 alias glg="git log --graph --pretty=format:'%Cred%h%Creset %Cblue%an%Creset: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
+alias gp='git push -u origin HEAD'
 alias glrp='glr && gp'
 alias gph='git push heroku HEAD'
 alias gd='git diff'
