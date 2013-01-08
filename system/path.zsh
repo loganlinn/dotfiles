22da094b3aa5eecd1363b86a81f54a4fcd82124b
loganlinn <logan.linn@gmail.com>
2013-01-08 10:47:38 -0800

    Add ~/bin to path

--- system/path.zsh
+++ system/path.zsh
@@ -1,5 +1,6 @@
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 export PATH="$PATH:$HOME/.rbenv/bin" #add rbenv
+export PATH="$PATH:$HOME/bin"
 export PATH="$PATH:/usr/local/go/bin"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
