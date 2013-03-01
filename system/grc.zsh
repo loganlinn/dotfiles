9a5b673b3e2d38412f1ec81704879855f009d129
loganlinn <logan.linn@gmail.com>
2013-02-28 16:19:48 -0800

    Ignore grc on non-OSX

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,4 +1,9 @@
 # GRC colorizes nifty unix tools all over the place
+if [[ "$(uname)" != 'Darwin' ]]
+then
+  exit
+fi
+
 if $(grc &>/dev/null) && $(brew &>/dev/null)
 then
   source `brew --prefix`/etc/grc.bashrc
