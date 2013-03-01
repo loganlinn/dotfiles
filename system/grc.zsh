fb638ede759ad7116c5ae5cbfec7e22428935754
loganlinn <logan.linn@gmail.com>
2013-02-28 16:46:31 -0800

    Dont use $PLATFORM here

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,7 +1,6 @@
 # GRC colorizes nifty unix tools all over the place
-[[ "$PLATFORM" == "osx" ]] || exit
 
-if $(grc &>/dev/null) && $(brew &>/dev/null)
+if [[ "$(uname -s)" = 'Darwin' ]] && $(grc &>/dev/null) && $(brew &>/dev/null)
 then
   source `brew --prefix`/etc/grc.bashrc
 fi
