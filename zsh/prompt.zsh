54a9246111268fea5d643b17e1df960d1fd0a711
loganlinn <logan.linn@gmail.com>
2013-03-05 16:14:54 -0800

    Update prompt.zsh

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,17 +2,24 @@ autoload colors && colors
 # cheers, @ehrenmurdick
 # http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
 
+if (( $+commands[git] ))
+then
+  git="$commands[git]"
+else
+  git="/usr/bin/git"
+fi
+
 git_branch() {
-  echo $(/usr/bin/git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
 }
 
 git_dirty() {
-  st=$(/usr/bin/git status 2>/dev/null | tail -n 1)
+  st=$($git status 2>/dev/null | tail -n 1)
   if [[ $st == "" ]]
   then
     echo ""
   else
-    if [[ $st == "nothing to commit (working directory clean)" ]]
+    if [[ "$st" =~ ^nothing ]]
     then
       echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
     else
@@ -22,13 +29,13 @@ git_dirty() {
 }
 
 git_prompt_info () {
- ref=$(/usr/bin/git symbolic-ref HEAD 2>/dev/null) || return
+ ref=$($git symbolic-ref HEAD 2>/dev/null) || return
 # echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
  echo "${ref#refs/heads/}"
 }
 
 unpushed () {
-  /usr/bin/git cherry -v @{upstream} 2>/dev/null
+  $git cherry -v @{upstream} 2>/dev/null
 }
 
 need_push () {
@@ -40,27 +47,41 @@ need_push () {
   fi
 }
 
-whoami_prompt(){
-    # username @ hostname
-    echo "%{$fg_bold[yellow]%}%n@%m%{$reset_color%}"
+rb_prompt(){
+  if (( $+commands[rbenv] ))
+  then
+	  echo "%{$fg_bold[yellow]%}$(rbenv version | awk '{print $1}')%{$reset_color%}"
+	else
+	  echo ""
+  fi
 }
 
-rb_prompt(){
-  if which rbenv &> /dev/null
+# This keeps the number of todos always available the right hand side of my
+# command line. I filter it to only count those tagged as "+next", so it's more
+# of a motivation to clear out the list.
+todo(){
+  if (( $+commands[todo.sh] ))
   then
-    echo "%{$fg_bold[yellow]%}$(rbenv version | awk '{print $1}')%{$reset_color%}"
+    num=$(echo $(todo.sh ls +next | wc -l))
+    let todos=num-2
+    if [ $todos != 0 ]
+    then
+      echo "$todos"
+    else
+      echo ""
+    fi
   else
-    whoami_prompt
+    echo ""
   fi
 }
 
 directory_name(){
-  echo "%{$fg_bold[cyan]%}%~%{$reset_color%}"
+  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-set_prompt(){
-  export PROMPT=$'\n$(whoami_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
-  export RPROMPT="%{$fg_bold[green]%}%@%{$reset_color%}"
+export PROMPT=$'\n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
+set_prompt () {
+  export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
 }
 
 precmd() {
