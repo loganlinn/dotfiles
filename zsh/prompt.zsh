bc0942ef030f070dcf467221393cf0a1a1c6210b
Logan Linn <logan.linn@gmail.com>
2013-04-06 17:42:22 -0700

    Use hostname in prompt, not current rbenv version

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -56,6 +56,10 @@ rb_prompt(){
   fi
 }
 
+hostname_prompt(){
+	echo "%{$fg_bold[yellow]%}$(hostname -s)%{$reset_color%}"
+}
+
 # This keeps the number of todos always available the right hand side of my
 # command line. I filter it to only count those tagged as "+next", so it's more
 # of a motivation to clear out the list.
@@ -79,7 +83,7 @@ directory_name(){
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n$(hostname_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
 }
