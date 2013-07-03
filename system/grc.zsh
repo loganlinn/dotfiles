a37424a1c7bca4d02033e29daab5259a750f8059
loganlinn <logan.linn@gmail.com>
2013-07-03 12:15:39 -0700

    Various cleanup, fixes

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,6 +1,6 @@
 # GRC colorizes nifty unix tools all over the place
 
-if [[ "$(uname -s)" = 'Darwin' ]] && $(grc &>/dev/null) && ! $(brew &>/dev/null)
+if [[ "$(uname -s)" == 'Darwin' ]] && $(grc &>/dev/null) && ! $(brew &>/dev/null)
 then
   source `brew --prefix`/etc/grc.bashrc
 fi
