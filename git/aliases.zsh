36780a3b821b7457e41a2f68d696de8136074723
loganlinn <logan.linn@gmail.com>
2018-08-29 18:34:09 -0700

    git updates

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,12 +1,3 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-hub_path=$(which hub 2>/dev/null)
-if [[ -f $hub_path ]]
-then
-  alias git=$hub_path
-fi
-
-# The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glo='gl && git pull origin'
 alias gfo='git fetch origin'
@@ -17,16 +8,18 @@ alias glrp='glr && gp'
 alias gd='git diff'
 alias gdc='git diff --cached'
 alias gc='git commit --verbose --no-verify'
-alias gca='git commit --all --verbose --no-verify'
+alias gcf='git commit --fixup --verbose --no-verify'
+alias gca='git commit --all --fixup --verbose --no-verify'
+alias gcaf='git commit --all --verbose --no-verify'
+alias gcb='git-copy-branch-name'
 alias gcm='git commit --amend --no-verify'
 alias gco='git checkout'
 alias gcob='git checkout -b'
-alias gb='git branch'
+alias gb='git switch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
            perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
-alias gstash='git stash -u'
 alias gw="git show"
-alias gss="git stash"
+alias gss="git stash -u"
 alias gsp="git stash pop"
 alias gsw="git stash show -p"
