d6e995de43d2bf9f476653371914daeb7264355c
loganlinn <logan.linn@gmail.com>
2018-11-10 14:31:07 -0800

    add git-clone helper function

---
+++ git/git-clone.zsh
@@ -0,0 +1,28 @@
+# git clone helper
+#
+# clones git repo and of its submodules into the specified directory,
+# then changes working directory. if directory not specified, uses PROJECTS,
+# and if that's not set, fallsback to current directory.
+# submodules fetched in parallel.
+#
+# requires: git 2.8+
+# author: logan@loganlinn.com
+function git-clone {
+  REPO="$1"
+  DIRECTORY="$2"
+
+  if [ -z "$REPO" ]; then
+    echo "usage: $0 <repository> [<directory>]"
+    exit 1
+  fi
+
+  REPONAME="$(basename $REPO)"
+  if [ -z "$DIRECTORY" ]; then
+    DIRECTORY="${PROJECTS:-$PWD}/${REPONAME%.git}"
+  fi
+
+  git clone --recurse-submodules --jobs 8 -- "$REPO" "$DIRECTORY"
+  cd $DIRECTORY
+}
+
+alias gclone="git-clone"
