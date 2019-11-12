57e22e73ee61fac67306463a900911ccbf1a77f2
loganlinn <logan.linn@gmail.com>
2019-11-12 10:51:38 -0800

    add ~/.local/bin to path

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:$HOME/bin:$ZSH/bin:$HOME/.rbenv/shims:$HOME/.cargo/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$PATH"
+export PATH="./bin:$HOME/bin:$ZSH/bin:$HOME/.local/bin:$HOME/.rbenv/shims:$HOME/.cargo/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
