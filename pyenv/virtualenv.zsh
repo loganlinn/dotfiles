95b98989174efa491f813f58fb9e5cda979a8266
loganlinn <logan.linn@gmail.com>
2019-11-28 02:07:34 -0800

    update act function

--- pyenv/virtualenv.zsh
+++ pyenv/virtualenv.zsh
@@ -3,7 +3,7 @@
 act() {
   local env="$1"
   if [[ ( -z "$env" ) || (! -d "$PYENV_ROOT/$env") ]]; then
-    env=$(pyenv virtualenvs | awk '/[-.0-9]+\/envs\//{next;}{print $1}' | fzf --select-1 --query="$env")
+    env=$(pyenv virtualenvs --bare | awk '/[-.0-9]+\/envs\//{next;}{print $1}' | fzf --select-1 --query="$env")
   fi
   pyenv activate "$env"
 }
