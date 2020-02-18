dd7777295a62c1bb5d406d0d731c5b56ba6c90bb
Logan Linn <logan.linn@gmail.com>
2020-02-18 11:49:31 +0700

    cleanup unused

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,3 @@
-export PATH="./bin:$HOME/bin:$ZSH/bin:$HOME/.local/bin:$HOME/.rbenv/shims:$HOME/.cargo/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$PATH"
+export PATH="$HOME/bin:$ZSH/bin:$HOME/.local/bin:$HOME/.rbenv/shims:$HOME/.cargo/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$PATH"
+
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
