263d5eab0282400303c03854653912444fdab09c
loganlinn <logan.linn@gmail.com>
2020-02-19 08:04:06 -0800

    update script/common.sh

--- script/common.sh
+++ script/common.sh
@@ -3,26 +3,47 @@
 set -eu
 
 cd "$(dirname "$0")/.."
-export DOTFILES=$(pwd -P)
+
+export DOTFILES="$(pwd -P)"
+
+# Only use colors if connected to a terminal
+if [ -t 1 ]; then
+  RED=$(printf '\033[31m')
+  GREEN=$(printf '\033[32m')
+  YELLOW=$(printf '\033[33m')
+  BLUE=$(printf '\033[34m')
+  BOLD=$(printf '\033[1m')
+  RESET=$(printf '\033[m')
+else
+  RED=""
+  GREEN=""
+  YELLOW=""
+  BLUE=""
+  BOLD=""
+  RESET=""
+fi
+
+command_exists() {
+  command -v "$@" >/dev/null 2>&1
+}
 
 info () {
-  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+  printf "\r  [ ${BLUE}..${RESET} ] %s\n" "$@"
 }
 
 user () {
-  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+  printf "\r  [ ${YELLOW}??${RESET} ] %s\n" "$@"
 }
 
 success () {
-  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+  printf "\r  [ ${GREEN}OK${RESET} ] %s\n" "$@"
 }
 
 warning () {
-  printf "\r\033[2K  [\033[0;33m!!\033[0m] $1\n"
+  printf "\r  [ ${YELLOW}!!${RESET} ] %s\n" "$@"
 }
 
 fail () {
-  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
-  echo ''
-  exit
+  printf "\r  [${RED}FAIL${RESET}] %s\n" "$@"
+  exit 1
 }
