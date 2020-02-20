ac72172105b3a4fa2b84e8dd1b91dad2d863170c
loganlinn <logan.linn@gmail.com>
2020-02-19 19:53:34 -0800

    linux: install nodejs, gh

--- script/bootstrap.linux.sh
+++ script/bootstrap.linux.sh
@@ -121,14 +121,19 @@ if ! command_exists go; then
   wget "https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz" 
   tar -C /usr/local -xzf "go${GO_VERSION}.linux-amd64.tar.gz" 
   rm -f "go${GO_VERSION}.linux-amd64.tar.gz"
-  export PATH="/usr/local/go/bin:$PATH"
+fi
+
+# install nodejs
+if ! command_exists node; then
+  curl -sL https://deb.nodesource.com/setup_13.x | bash -
+  apt-get install -y nodejs
 fi
 
 # install 1password
 if ! command_exists op; then
   OP_VERSION="v0.5.6-003"
   info "installing op ($OP_VERSION)"
-  curl -sS -o 1password.zip https://cache.agilebits.com/dist/1P/op/pkg/${OP_VERSION}/op_linux_amd64_${OP_VERSION}.zip
+  curl -sS -o 1password.zip "https://cache.agilebits.com/dist/1P/op/pkg/${OP_VERSION}/op_linux_amd64_${OP_VERSION}.zip"
   unzip 1password.zip op -d /usr/local/bin
   rm -f 1password.zip
 fi
@@ -137,40 +142,51 @@ fi
 if ! command_exists doctl; then
   DOCTL_VERSION="1.20.1"
   info "installing doctl ($DOCTL_VERSION)"
-  wget https://github.com/digitalocean/doctl/releases/download/v${DOCTL_VERSION}/doctl-${DOCTL_VERSION}-linux-amd64.tar.gz
-  tar xf doctl-${DOCTL_VERSION}-linux-amd64.tar.gz 
-  chmod +x doctl 
-  mv doctl /usr/local/bin 
-  rm -f doctl-${DOCTL_VERSION}-linux-amd64.tar.gz
+  wget "https://github.com/digitalocean/doctl/releases/download/v${DOCTL_VERSION}/doctl-${DOCTL_VERSION}-linux-amd64.tar.gz"
+  tar xf "doctl-${DOCTL_VERSION}-linux-amd64.tar.gz"
+  chmod +x doctl
+  mv doctl /usr/local/bin
+  rm -f "doctl-${DOCTL_VERSION}-linux-amd64.tar.gz"
 fi
 
 # install terraform
 if ! command_exists terraform; then
   TERRAFORM_VERSION="0.12.9"
   info "installing terraform ($TERRAFORM_VERSION)"
-  wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip 
-  unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip 
+  wget "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+  unzip "terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
   chmod +x terraform
   mv terraform /usr/local/bin
-  rm -f terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+  rm -f "terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
 fi
 
 # install hub
 if ! command_exists hub; then
   HUB_VERSION="2.12.3"
   info "installing hub ($HUB_VERSION)"
-  wget https://github.com/github/hub/releases/download/v${HUB_VERSION}/hub-linux-amd64-${HUB_VERSION}.tgz
-  tar xf hub-linux-amd64-${HUB_VERSION}.tgz
-  chmod +x hub-linux-amd64-${HUB_VERSION}/bin/hub
-  cp hub-linux-amd64-${HUB_VERSION}/bin/hub /usr/local/bin
-  rm -rf hub-linux-amd64-${HUB_VERSION}
-  rm -f hub-linux-amd64-${HUB_VERSION}.tgz*
+  wget "https://github.com/github/hub/releases/download/v${HUB_VERSION}/hub-linux-amd64-${HUB_VERSION}.tgz"
+  tar xf "hub-linux-amd64-${HUB_VERSION}.tgz"
+  chmod +x "hub-linux-amd64-${HUB_VERSION}/bin/hub"
+  cp "hub-linux-amd64-${HUB_VERSION}/bin/hub" /usr/local/bin
+  rm -rf "hub-linux-amd64-${HUB_VERSION}"
+  rm -f "hub-linux-amd64-${HUB_VERSION}.tgz*"
 fi
 
+# install gh
+if ! command_exists gh; then
+  GH_VERSION="0.5.5"
+  info "installing gh ($GH_VERSION)"
+  rm -f "gh_*_linux_amd64.deb"
+  wget "https://github.com/cli/cli/releases/download/v${GH_VERSION}/gh_${GH_VERSION}_linux_amd64.deb"
+  dpkg -i "gh_${GH_VERSION}_linux_amd64.deb"
+  rm -f "gh_${GH_VERSION}_linux_amd64.deb"
+fi
+
+# install fd
 if ! command_exists fd; then
   FD_VERSION="7.4.0"
   info "installing fd ($FD_VERSION)"
-  wget https://github.com/sharkdp/fd/releases/download/v${FD_VERSION}/fd_${FD_VERSION}_amd64.deb
+  wget "https://github.com/sharkdp/fd/releases/download/v${FD_VERSION}/fd_${FD_VERSION}_amd64.deb"
   dpkg -i fd_${FD_VERSION}_amd64.deb
   rm -f fd_${FD_VERSION}_amd64.deb
 fi
