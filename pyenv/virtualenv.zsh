31262fd83b87faa6f584ebb3b1c25dc377392079
loganlinn <logan.linn@gmail.com>
2020-03-02 13:44:05 -0800

    update pyenv/virtualenv.zsh

--- pyenv/virtualenv.zsh
+++ pyenv/virtualenv.zsh
@@ -1,11 +1,35 @@
 
-# activate a virtualenv or use fzf to select one
+#
+# act: activates a pyenv-managed virtualenv based on args, pwd, or fzf choice
+#
 act() {
   local env="$1"
-  if [[ ( -z "$env" ) || (! -d "$PYENV_ROOT/$env") ]]; then
-    env=$(pyenv virtualenvs --bare | awk '/[-.0-9]+\/envs\//{next;}{print $1}' | fzf --select-1 --query="$env")
+
+  if [[ "$#" -ne "0" ]]; then
+    pyenv activate "$1"
+    return 0
   fi
-  pyenv activate "$env"
+
+  if [[ -z "$VIRTUAL_ENV" ]]; then
+    project=$(basename $(git rev-parse --show-toplevel 2>/dev/null || pwd))
+    if [[ -d "$PYENV_ROOT/versions/$project" ]]; then
+      pyenv activate "$project"
+      return 0
+    fi
+  fi
+
+  choice=$(pyenv virtualenvs --bare | awk '/[-.0-9]+\/envs\//{next;}{print $1}' | fzf --select-1 --query="$env")
+  if [[ -n "$choice" ]]; then
+    pyenv activate "$choice"
+    return 0
+  fi
+
+  return 1
+}
+
+act1() {
+    project=$(basename $(git rev-parse --show-toplevel 2>/dev/null || pwd))
+    pyenv activate "$project" || pyenv virtualenv "$project" && pyenv activate "$project"
 }
 
 
