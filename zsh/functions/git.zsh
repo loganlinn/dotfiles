1ca4d98105a1d30d74f0b8d80228c870d62200a6
loganlinn <logan@loganlinn.com>
2020-11-24 15:06:34 -0800

    remove gco alias override (pwnd by git checkout -B)

--- zsh/functions/git.zsh
+++ zsh/functions/git.zsh
@@ -1,22 +1,3 @@
-unalias gco
-
-# @name gco
-# @description checkout branch if given, otherwise choose interactively from recent branches
-# @arg $1 string branch
-# @arg $@ any git-checkout args
-function gco() {
-  if [ $# -eq 0 ]; then
-    local branches branch
-    branches="$(git branch --sort=-committerdate)" &&
-      branch="$(echo "$branches" | fzf-tmux -d 15 +m)"
-    if [ $? -eq 0 ]; then
-      git checkout "$(echo "$branch" | sed "s/.* //")"
-    fi
-  else
-    git checkout -B "$@"
-  fi
-}
-
 function gitignore.io() {
   http GET "https://www.gitignore.io/api/$@" --follow --body
 }
