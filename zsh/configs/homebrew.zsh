0fd83b8ad055f9de290e522e837e14ee700352f7
loganlinn <logan@loganlinn.com>
2021-01-29 17:20:12 -0800

    fix Brewfile.lock.json separation from Brewfile

--- zsh/configs/homebrew.zsh
+++ zsh/configs/homebrew.zsh
@@ -1,2 +1,5 @@
+# dnt
 export HOMEBREW_NO_ANALYTICS=1
-export HOMEBREW_BUNDLE_FILE=$HOME/.Brewfile
+
+# resolve symlink so that Brewfile.lock.json written relativeto real file
+export HOMEBREW_BUNDLE_FILE=$(realpath $HOME/.Brewfile)
