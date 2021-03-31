703a6354a673a6b729ebd1838926f59b82f44d0f
loganlinn <logan@loganlinn.com>
2021-03-30 17:54:54 -0700

    asdf.sh improvements

--- hooks/post-up/asdf.sh
+++ hooks/post-up/asdf.sh
@@ -1,63 +1,55 @@
 #!/usr/bin/env bash
 
-[[ ${TRACE-} =~ ^1|yes|true$ ]] && set -o xtrace
-set -o errexit
-set -o nounset
-set -o pipefail
-
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+[[ ${TRACE-} =~ ^1|yes|true$ ]] && set -x
+set -euo pipefail
 
 ASDF_DIR=${ASDF_DIR:-$HOME/.asdf}
 
-ASDF_PLUGINS_PATHS=(
-  "${SCRIPT_DIR}/../../asdf.plugins"
-  ~/.asdf.plugins.local
-)
-
-function git-checkout-latest-tag() {
-	local git_dir=$1
-	local tag
-	tag=$(git -C "$git_dir" describe --abbrev=0 --tags)
-	git -C "$git_dir" checkout tags/"$tag" -B "$tag"
+function install_asdf() {
+	if [[ ! -d ${ASDF_DIR?} ]]; then
+		git clone https://github.com/asdf-vm/asdf.git "$ASDF_DIR"
+		(
+			cd "$ASDF_DIR"
+			tag=$(git describe --abbrev=0 --tags)
+			git checkout -q "tags/${tag}" -b "$tag"
+		)
+	fi
 }
 
-function setup_asdf_dir() {
-  if [[ ! -d $ASDF_DIR ]]; then
-    git clone https://github.com/asdf-vm/asdf.git "$ASDF_DIR"
-    git-checkout-latest-tag "$ASDF_DIR"
-  fi
+function load_asdf_plugin_files() {
+	for f in "$@"; do
+		if [[ -f "$f" ]]; then
+			<"$f" _strip_comments | _strip_existing_plugins | xargs -r -t -L1 asdf plugin-add
+		fi
+	done
 }
 
-# add asdf plugins, ignoring  those that are not currently installed
-function asdf-plugin-up() {
-  sed "/^$(asdf plugin-list | sed ':a; N; $!ba; s/\n/\\|/g')/d" \
-    | xargs -t asdf plugin-add
+function _strip_existing_plugins() {
+	local -r plugins=$(asdf plugin-list)
+
+	if [[ -n $plugins ]]; then
+		sed "/^${plugins//$'\n'/\\|}/d"
+	else
+		cat -
+	fi
 }
 
-function setup_asdf_plugins() {
-  for f in ${ASDF_PLUGINS_PATHS[*]}; do
-    if [[ -f $f ]]; then
-      asdf-plugin-up < "$f"
-    fi
-  done
-}
-
-function reload_asdf() {
-	# shellcheck source=../../../.asdf/asdf.sh
-  source "${ASDF_DIR}/asdf.sh"
-}
-
-function print_asdf_summary() {
-  printf "%s:\\n%s\\n\\n" "ASDF VERSION" "$(asdf version)"
-  printf "%s:\\n%s\\n\\n" "ASDF ENVIRONMENT VARIABLES" "$(env | grep -E "^ASDF_*")"
-  printf "%s:\\n%s\\n\\n" "ASDF INSTALLED PLUGINS" "$(asdf plugin list --urls)"
+function _strip_comments() {
+	grep -v '#'
 }
 
 function main() {
-  setup_asdf_dir
-  setup_asdf_plugins
-	reload_asdf
-  print_asdf_summary
+	install_asdf
+
+	# shellcheck source=/dev/null
+	source "${ASDF_DIR}/asdf.sh"
+
+	load_asdf_plugin_files ~/.asdf.plugins ~/.asdf.plugins.local
+
+	# print summary
+	printf "%s:\\n%s\\n\\n" "ASDF VERSION" "$(asdf version)"
+	printf "%s:\\n%s\\n\\n" "ASDF ENVIRONMENT VARIABLES" "$(env | grep -E "^ASDF_*")"
+	printf "%s:\\n%s\\n\\n" "ASDF INSTALLED PLUGINS" "$(asdf plugin list --urls)"
 }
 
 main
