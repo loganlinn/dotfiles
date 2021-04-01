413b1dee6b915cf65283593b5ecd9073b7f76c83
loganlinn <logan.linn@gmail.com>
2021-03-31 21:58:08 -0700

    replace use of realpath command

--- zsh/configs/homebrew.zsh
+++ zsh/configs/homebrew.zsh
@@ -2,4 +2,4 @@
 export HOMEBREW_NO_ANALYTICS=1
 
 # resolve symlink so that Brewfile.lock.json written relativeto real file
-export HOMEBREW_BUNDLE_FILE=$(realpath $HOME/.Brewfile)
+export HOMEBREW_BUNDLE_FILE="$(dirname "$0:A")/../../tag-darwin/Brewfile"
