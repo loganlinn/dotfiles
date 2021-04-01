2d2a0afab962cedd9abe21a66f0369ccb358c1f9
loganlinn <logan@loganlinn.com>
2021-03-31 17:08:25 -0700

    asdf-up

--- hooks/post-up/asdf-up.sh
+++ hooks/post-up/asdf-up.sh
@@ -14,17 +14,6 @@ set -euo pipefail
 
 ASDF_DIR=${ASDF_DIR:-$HOME/.asdf}
 
-function asdf_dir_bootstrap() {
-	if [[ ! -d ${ASDF_DIR?} ]]; then
-		git clone https://github.com/asdf-vm/asdf.git "$ASDF_DIR"
-		(
-			cd "$ASDF_DIR"
-			tag=$(git describe --abbrev=0 --tags)
-			git checkout -q "tags/${tag}" -b "$tag"
-		)
-	fi
-}
-
 # @brief adds asdf plugins that are not already registered
 # @description
 #     - Reads arguments to `asdf plugin-add` from stdin or file argument.
@@ -33,31 +22,37 @@ function asdf_dir_bootstrap() {
 #     - Runs `asdf plugin-add` on the rest
 asdf_plugins_init() {
 	local -r ignore_prefixes=$(printf '# %s' "$(asdf plugin-list)" | xargs echo)
-	sed "/^${ignore_prefixes// /\\|}/d" "${1-}" | xargs -r -t -L1 asdf plugin-add
+	sed -e "/^${ignore_prefixes// /\\|}/d" "${1-}" | xargs -r -t -L1 asdf plugin-add
 }
 
-function strong_echo() {
-	echo -e "\033[1m${*}\033[m"
+# shellcheck disable=SC2001
+function info_section() {
+  local -r heading=$1
+	>&2 echo -e "\033[1m${heading}\033[m"
+  >&2 echo
+  >&2 sed -e "s/^/    /"
+  >&2 echo
 }
 
 function main() {
-	asdf_dir_bootstrap
+	if ! [[ -d $ASDF_DIR ]]; then
+		git clone https://github.com/asdf-vm/asdf.git "$ASDF_DIR"
+  fi
 
 	# shellcheck source=/dev/null
 	source "${ASDF_DIR}/asdf.sh"
+
+  asdf update 2>/dev/null
 
 	# initialize plugins
 	if [[ -f ~/.asdf-plugins ]]; then
 		asdf_plugins_init ~/.asdf-plugins
 	fi
 
-	local -r pad="    "
-	strong_echo ASDF VERSION
-	printf "\n%s\n\n" "${pad}$(asdf version)"
-	strong_echo "ASDF ENVIRONMENT VARIABLES"
-	printf "\n%s\n\n" "$(env | grep '^ASDF_' | sed "s/^/${pad}/")"
-	strong_echo "ASDF PLUGINS:"
-	printf "\n%s\n\n" "$(asdf plugin list --urls | sed "s/^/${pad}/")"
+  # print asdf info
+  asdf version | info_section "ASDF VERSION"
+  env | grep '^ASDF_' | info_section "ASDF ENVIRONMENT VARIABLES"
+  asdf plugin list --urls | info_section "ASDF PLUGINS"
 }
 
 main
