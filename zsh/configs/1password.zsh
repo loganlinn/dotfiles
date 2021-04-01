8fdc6e92e3a6afe3845cd79716f60d8041e98468
loganlinn <logan.linn@gmail.com>
2021-03-31 22:31:18 -0700

    update do-api-token alias

--- zsh/configs/1password.zsh
+++ zsh/configs/1password.zsh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
 
-alias get-digitalocean-token='1ps get item f6mb6bqqjzf5jhrm4hds34uqlu --fields password'
+# digital-ocean
+alias 1ps:do-api-token='1ps get item f6mb6bqqjzf5jhrm4hds34uqlu --fields password'
