b68c33d07bd5a54d3c6a94613ca727c649fb9a6f
loganlinn <logan@loganlinn.com>
2021-06-24 13:05:01 -0700

    moar history!

--- zsh/configs/history.zsh
+++ zsh/configs/history.zsh
@@ -1,6 +1,7 @@
 setopt hist_ignore_all_dups inc_append_history
-HISTFILE=~/.zhistory
-HISTSIZE=4096
-SAVEHIST=4096
+
+HISTFILE=$HOME/.zhistory
+HISTSIZE=65536
+SAVEHIST=65536
 
 export ERL_AFLAGS="-kernel shell_history enabled"
