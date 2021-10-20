bc88b055a9730463d0c99b1aa309557df4dc2c5d
loganlinn <logan@loganlinn.com>
2021-10-20 12:20:09 -0700

    dont error if asdf or direnv not installed

--- zsh/plugins/asdf-direnv/asdf-direnv.plugin.zsh
+++ zsh/plugins/asdf-direnv/asdf-direnv.plugin.zsh
@@ -1,5 +1,8 @@
 #!/usr/bin/env zsh
 
-eval "$(asdf exec direnv hook zsh)"
-
-direnv() { asdf exec direnv "$@"; }
+if (($+commands[asdf])); then
+  if (($+commands[direnv])); then
+    eval "$(asdf exec direnv hook zsh)"
+    direnv() { asdf exec direnv "$@"; }
+  fi
+fi
