d538d2bcf48a8468850b3aa4448164d2ef00622d
loganlinn <logan@loganlinn.com>
2021-10-28 10:10:49 -0700

    dont run tpm update_plugins every time

--- hooks/post-up/install-tpm.sh
+++ hooks/post-up/install-tpm.sh
@@ -2,8 +2,7 @@
 
 TPM_ROOT=${TPM_ROOT:-$HOME/.tmux/plugins/tpm}
 
-if ! [[ -d $TPM_ROOT ]]; then 
-  git clone https://github.com/tmux-plugins/tpm.git "${TPM_ROOT}"
+if ! [[ -d $TPM_ROOT ]]; then
+	git clone https://github.com/tmux-plugins/tpm.git "${TPM_ROOT}"
+	"${TPM_ROOT}/bin/install_plugins"
 fi
-
-exec "${TPM_ROOT}/bin/install_plugins"
