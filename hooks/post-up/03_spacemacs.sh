86f74a5d03e916ef32519fe073f10213a9c3b809
loganlinn <logan@loganlinn.com>
2021-11-03 16:19:02 -0700

    numbered hooks

--- hooks/post-up/03_spacemacs.sh
+++ hooks/post-up/03_spacemacs.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
 if ! git -C ~/.emacs.d rev-parse 2>/dev/null; then
-	echo "Installing spacemacs..."
+  if [ -d ~/.emacs.d ]; then
+    mv -v -b ~/.emacs.d ~/.emacs.d~
+  fi
+	echo "Installing Spacemacs..." >&2
 	git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 fi
