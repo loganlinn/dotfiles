09eafeff73db3ccc860b1ecb1e6ec79956a97f9f
Logan Linn <logan@llinn.dev>
2022-02-17 13:22:40 -0800

    fixup! latest shell updates

---
+++ zsh/configs-post/antigen.zsh
@@ -0,0 +1,12 @@
+export ANTIGEN=$HOME/antigen
+export ADOTDIR=$HOME/.antigen
+export ZSH=$ADOTDIR/bundles/robbyrussell/oh-my-zsh
+export ZSH_CUSTOM=$HOME/.zsh
+
+[[ -d $ANTIGEN ]]    || git clone https://github.com/zsh-users/antigen.git "$ANTIGEN"
+[[ -d $ZSH ]]        || git clone https://github.com/ohmyzsh/ohmyzsh.git "$ZSH"
+[[ -d $ZSH_CUSTOM ]] || rcup -v zsh
+
+source "$ANTIGEN/antigen.zsh"
+
+antigen init "$HOME/.antigenrc"
