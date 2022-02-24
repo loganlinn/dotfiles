fb3761b66bdd41224540d7c109ad70d994e7a22a
Logan Linn <logan@llinn.dev>
2022-02-23 18:54:10 -0800

    uhh, zsh history settings

--- zsh/configs/history.zsh
+++ zsh/configs/history.zsh
@@ -1,7 +1,11 @@
-setopt hist_ignore_all_dups inc_append_history
-
-HISTFILE=$HOME/.zhistory
+HISTFILE="$HOME/.zsh_history"
 HISTSIZE=65536
 SAVEHIST=65536
 
-export ERL_AFLAGS="-kernel shell_history enabled"
+setopt EXTENDED_HISTORY         # Record timestamp of command in HISTFILE
+setopt HIST_EXPIRE_DUPS_FIRST   # Delete duplicates first when HISTFILE size exceeds HISTSIZE
+setopt HIST_IGNORE_DUPS         # Ignore duplicated commands history list
+setopt HIST_IGNORE_SPACE        # Ignore commands that start with space
+setopt HIST_VERIFY              # Show command with history expansion to user before running it
+setopt INC_APPEND_HISTORY       # Append to the history file after command executed, not just when a term is killed
+setopt SHARE_HISTORY            # Share command history data
