67c3f459286ff864d53d3c8406a51f13b8368d2c
Logan Linn <logan@llinn.dev>
2022-12-07 01:14:34 -0800

    prep hardward-config.nix

--- nix/hardware/nijusan.nix
+++ nix/hardware/nijusan.nix
@@ -5,4 +5,22 @@
   modulesPath,
   ...
 }: {
+
+  boot.kernelParams = [
+    "i915.enable_psr=1"
+    "i915.force_probe=a780"
+  ];
+
+  # nixpkgs.config.packageOverrides = pkgs: {
+  #   vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true;  };
+  # };
+  # hardware.opengl = {
+  #   enable = true;
+  #   extraPackages = with pkgs; [
+  #     intel-media-driver # LIBVA_DRIVER_NAME=iHD
+  #     vaapiIntel         # LIBVA_DRIVER_NAME=i964
+  #     vaapiVdpau
+  #     libvdpau-va-gl
+  #   ];
+  # };
 }
