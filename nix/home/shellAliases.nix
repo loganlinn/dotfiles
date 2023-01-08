c9536011e71d889225b62808e2ebdbfab82f104e
Logan Linn <logan@llinn.dev>
2023-01-07 19:42:10 -0800

    tidy

--- nix/home/shellAliases.nix
+++ nix/home/shellAliases.nix
@@ -1,14 +1,10 @@
-{
-  conf,
-  lib,
-  pkgs,
-  ...
-}: {
+{ conf, lib, pkgs, ... }: {
   home.shellAliases = let
+    inherit (lib) getExe;
     inherit (pkgs.stdenv.hostPlatform) isDarwin;
-    gitBin = "${pkgs.git}/bin/git";
-    homeManagerBin = "${pkgs.home-manager}/bin/home-manager";
-  in {
+    git = getExe pkgs.git;
+    kitty = getExe pkgs.kitty;
+  in rec {
     "'..'" = "cd ..";
     "'...'" = "cd ...";
 
@@ -18,48 +14,43 @@
     l = "ls -lah";
     mkd = "mkdir -p";
 
-    gc = "${gitBin} commit -v";
-    gca = "${gitBin} commit -v -a";
-    gco = "${gitBin} switch";
-    gcm = "${gitBin} switch \"$(${gitBin} default-branch || echo .)\"";
-    gcob = "${gitBin} switch -c";
-    gcop = "${gitBin} checkout -p";
-    gd = "${gitBin} diff --color";
-    gdc = "${gitBin} diff --color --cached";
-    gfo = "${gitBin} fetch origin";
-    gl = "${gitBin} pull";
-    glr = "${gitBin} pull --rebase";
-    glrp = "glr && gp";
-    gp = "${gitBin} push -u";
-    gpa = "${gitBin} push all --all";
-    gs = "${gitBin} status -sb";
-    gsrt = "${gitBin} rev-parse --show-toplevel";
-    gsw = "${gitBin} stash show --patch";
-    gw = "${gitBin} show";
-    grt = ''cd -- "$(${gitBin} rev-parse --show-top-level || echo .)"'';
+    gc = "${git} commit -v";
+    gca = "${git} commit -v -a";
+    gco = "${git} switch";
+    gcm = ''${git} switch "$(${git} default-branch || echo .)"'';
+    gcob = "${git} switch -c";
+    gcop = "${git} checkout -p";
+    gd = "${git} diff --color";
+    gdc = "${git} diff --color --cached";
+    gfo = "${git} fetch origin";
+    gl = "${git} pull";
+    glr = "${git} pull --rebase";
+    glrp = "${glr} && ${gp}";
+    gp = "${git} push -u";
+    gpa = "${git} push all --all";
+    gs = "${git} status -sb";
+    gsrt = "${git} rev-parse --show-toplevel";
+    gsw = "${git} stash show --patch";
+    gw = "${git} show";
+    grt = ''cd -- "$(${git} rev-parse --show-top-level || echo .)"'';
 
-    nix-gc = "${pkgs.nix}/bin/nix-collect-garbage -d";
-    nixq = "${pkgs.nix}/bin/nix-env -qaP";
-    hm = "${homeManagerBin}";
+    nix-gc = "nix-collect-garbage -d";
+    nixq = "nix-env -qaP";
+    hm = getExe pkgs.home-manager;
 
-    switch =
-      if isDarwin
-      then "darwin-rebuild switch --impure --flake ~/.dotfiles#\${USER?}@\${HOST?}"
-      else "${homeManagerBin} switch --flake ~/.dotfiles#\${USER?}@\${HOST?}";
+    switch = if isDarwin then
+      "darwin-rebuild switch --impure --flake ~/.dotfiles#\${USER?}@\${HOST?}"
+    else
+      "${hm} switch --flake ~/.dotfiles#\${USER?}@\${HOST?}";
 
-    k = "${pkgs.kubectl}/bin/kubectl";
-    kctx = "${pkgs.kubectx}/bin/kubectx";
-    kusers = "k config get-users";
-    kdesc = "k describe";
-    kdoc = "k describe";
-    kinfo = "k cluster-info";
-    kcfg = "k config view --raw";
-    kk = "${pkgs.kustomize}/bin/kustomize";
-    kkb = "kk build";
+    k = getExe pkgs.kubectl;
+    kctx = getExe pkgs.kubectx;
+    kk = getExe pkgs.kustomize;
+    kkb = "${kk} build";
 
     # s = "${pkgs.kitty}/bin/kitty +kitten ssh";
-    s = "kitty +kitten ssh";
+    s = "${kitty} +kitten ssh";
 
-    bbr = "${pkgs.rlwrap}/bin/rlwrap bb";
+    bb = "${getExe pkgs.rlwrap} ${getExe pkgs.babashka}";
   };
 }
