9cd5317f2ad039ac1ea7229d4edc9373e7c10d4d
Logan Linn <logan@llinn.dev>
2023-01-08 19:13:11 -0800

    powermenu

--- nix/home/rofi.nix
+++ nix/home/rofi.nix
@@ -1,19 +1,24 @@
-{ config, lib, pkgs, ... }:
-
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 with lib;
+  mkIf pkgs.stdenv.targetPlatform.isLinux (let
+    rofi = config.programs.rofi.finalPackage;
+    kitty = config.programs.kitty.package;
+    powermenu = pkgs.writeShellScriptBin "powermenu" ''
+      ${getExe rofi} -show p -modi p:${getExe pkgs.rofi-power-menu} -width 20 -lines 6
+    '';
+  in {
+    programs.rofi = {
+      enable = true;
+      pass.enable = true;
+      terminal = getExe kitty;
+      font = "Victor Mono Regular";
+      plugins = with pkgs; [rofi-file-browser rofi-calc];
+    };
 
-mkIf pkgs.stdenv.targetPlatform.isLinux {
-  programs.rofi = {
-    enable = true;
-    pass.enable = true;
-    terminal = getExe config.programs.kitty.package;
-    font = "Victor Mono Regular";
-    plugins = with pkgs; [ rofi-file-browser rofi-calc ];
-  };
-  home.packages = with pkgs; [
-    rofi-power-menu
-    (writeShellScriptBin "powermenu" ''
-      rofi -show p -modi p:rofi-power-menu -width 20 -lines 6
-    '')
-  ];
-}
+    home.packages = with pkgs; [powermenu];
+  })
