436b65d2b2a4417a1fdee6804f4c644f9960c326
Logan Linn <logan@llinn.dev>
2023-01-09 16:32:35 -0800

    prefer to use latest over purity

--- nix/home/shellAliases.nix
+++ nix/home/shellAliases.nix
@@ -1,9 +1,6 @@
 { conf, lib, pkgs, ... }: {
   home.shellAliases = let
-    inherit (lib) getExe;
     inherit (pkgs.stdenv.targetPlatform) isDarwin;
-    git = getExe pkgs.git;
-    kitty = getExe pkgs.kitty;
   in rec {
     "'..'" = "cd ..";
     "'...'" = "cd ...";
@@ -12,37 +9,37 @@
     l = "ls -lah";
     mkd = "mkdir -p";
 
-    gc = "${git} commit -v";
-    gca = "${git} commit -v -a";
-    gco = "${git} switch";
-    gcm = ''${git} switch "$(${git} default-branch || echo .)"'';
-    gcob = "${git} switch -c";
-    gcop = "${git} checkout -p";
-    gd = "${git} diff --color";
-    gdc = "${git} diff --color --cached";
-    gfo = "${git} fetch origin";
-    gl = "${git} pull";
-    glr = "${git} pull --rebase";
+    gc = "git commit -v";
+    gca = "git commit -v -a";
+    gco = "git switch";
+    gcm = ''git switch "$(git default-branch || echo .)"'';
+    gcob = "git switch -c";
+    gcop = "git checkout -p";
+    gd = "git diff --color";
+    gdc = "git diff --color --cached";
+    gfo = "git fetch origin";
+    gl = "git pull";
+    glr = "git pull --rebase";
     glrp = "${glr} && ${gp}";
-    gp = "${git} push -u";
-    gpa = "${git} push all --all";
-    gs = "${git} status -sb";
-    gsrt = "${git} rev-parse --show-toplevel";
-    gsw = "${git} stash show --patch";
-    gw = "${git} show";
-    grt = ''cd -- "$(${git} rev-parse --show-top-level || echo .)"'';
+    gp = "git push -u";
+    gpa = "git push all --all";
+    gs = "git status -sb";
+    gsrt = "git rev-parse --show-toplevel";
+    gsw = "git stash show --patch";
+    gw = "git show";
+    grt = ''cd -- "$(git rev-parse --show-top-level || echo .)"'';
 
     nix-gc = "nix-collect-garbage -d";
     nixq = "nix-env -qaP";
-    hm = getExe pkgs.home-manager;
+    hm = "home-manager";
 
-    k = getExe pkgs.kubectl;
-    kctx = getExe pkgs.kubectx;
-    kk = getExe pkgs.kustomize;
+    k = "kubectl";
+    kctx = "kubectx";
+    kk = "kustomize";
     kkb = "${kk} build";
 
-    s = "${kitty} +kitten ssh";
+    s = "kitty +kitten ssh";
 
-    bb = "${getExe pkgs.rlwrap} ${getExe pkgs.babashka}";
+    bb = "rlwrap bb";
   };
 }
