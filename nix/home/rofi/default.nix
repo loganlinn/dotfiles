3a084f8073195d6a36d1af7a4011edc4f82ed9ff
Logan Linn <logan@llinn.dev>
2023-01-16 13:45:25 -0800

    rofi

--- nix/home/rofi/default.nix
+++ nix/home/rofi/default.nix
@@ -1,32 +1,32 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
+{ config
+, lib
+, pkgs
+, ...
 }:
 with lib;
-  mkIf pkgs.stdenv.targetPlatform.isLinux {
-    programs.rofi = {
-      enable = mkDefault true;
-      pass.enable = mkIf config.programs.password-store.enable true;
-      font = mkDefault "DejaVu Sans Mono";
-      plugins = with pkgs; [
-        rofi-calc
-        rofi-emoji
-        rofi-file-browser
-      ];
-    };
-
-    home.packages = with pkgs; [
-      (writeShellApplication {
-        name = "powermenu";
-        runtimeInputs = with pkgs; [
-          config.programs.rofi.finalPackage
-          rofi-power-menu
-        ];
-        text = ''
-          rofi -show p -modi p:rofi-power-menu -width 20 -lines 6
-        '';
-      })
+mkIf pkgs.stdenv.targetPlatform.isLinux {
+  programs.rofi = {
+    enable = mkDefault true;
+    pass.enable = mkIf config.programs.password-store.enable true;
+    font = mkDefault "DejaVu Sans Mono";
+    plugins = with pkgs; [
+      rofi-calc
+      rofi-emoji
+      rofi-file-browser
+      rofi-pulse-select
     ];
-  }
+  };
+
+  home.packages = with pkgs; [
+    (writeShellApplication {
+      name = "powermenu";
+      runtimeInputs = with pkgs; [
+        config.programs.rofi.finalPackage
+        rofi-power-menu
+      ];
+      text = ''
+        rofi -show p -modi p:rofi-power-menu -width 20 -lines 6
+      '';
+    })
+  ];
+}
