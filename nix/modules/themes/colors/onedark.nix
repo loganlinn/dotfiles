91686040fbfa3dfc73e5a1ede073b19a432c71df
Logan Linn <logan@llinn.dev>
2023-01-18 11:32:55 -0800

    the colors, nix!

---
+++ nix/modules/themes/colors/onedark.nix
@@ -0,0 +1,22 @@
+# https://github.com/joshdick/onedark.vim/blob/main/autoload/onedark.vim
+rec {
+  black = "#282C34";
+  white = "#ABB2BF";
+  background = black;
+  foreground = white;
+  lightRed = "#E06C75";
+  darkRed = "#BE5046";
+  green = "#98C379";
+  lightYellow = "#E5C07B";
+  darkYellow = "#D19A66";
+  blue = "#61AFEF";
+  magenta = "#C678DD";
+  cyan = "#56B6C2";
+  gutterGrey = "#4B5263";
+  commentGrey = "#5C6370";
+  cursorGrey = "#2C323C";
+  visualGrey = "#3E4452";
+  menuGrey = "#3E4452";
+  specialGrey = "#3B4048";
+  vertSplit = "#3E4452";
+}
