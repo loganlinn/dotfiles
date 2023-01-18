6a8988fd9b2ee69addc6acc0c692dd8d4039f578
Logan Linn <logan@llinn.dev>
2023-01-17 16:35:02 -0800

    add color lib
    h/t https://github.com/bertof/nix-rice/blob/ac810023c1bb3d999ab4295ea4eb261412f4b7c0

--- nix/lib/default.nix
+++ nix/lib/default.nix
@@ -1,8 +1,13 @@
-{ inputs, lib, pkgs, ... }:
+{ lib
+, pkgs
+, ...
+} @ args:
+
 let
   inherit (lib) attrValues makeExtensible mergeAttrs foldr;
   inherit (modules) mapModules;
 
+  # Bootstrap with modules.nix...
   modules = import ./modules.nix {
     inherit lib pkgs;
     self.attrs = import ./attrs.nix {
@@ -11,11 +16,14 @@ let
     };
   };
 
-  mylib = makeExtensible (self:
-    with self;
-    mapModules ./. (file:
-      import file {
-        inherit self lib pkgs inputs;
-      }));
+  # Import the rest of ./*.nix
+  mylib = makeExtensible
+    (self:
+      with self;
+      mapModules ./. (file:
+        import file (args // {
+          inherit self lib pkgs inputs;
+        })
+      ));
 in
 mylib.extend (self: super: foldr mergeAttrs { } (attrValues super))
