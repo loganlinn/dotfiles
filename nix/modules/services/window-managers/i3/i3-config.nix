14c537fd1ee91556439a618eb85615d807e793bd
Logan Linn <logan@llinn.dev>
2023-02-01 15:23:31 -0800

    sort floating criteria

--- nix/modules/services/window-managers/i3/i3-config.nix
+++ nix/modules/services/window-managers/i3/i3-config.nix
@@ -321,23 +321,23 @@ rec {
 
   floating = {
     criteria = [
+      { class = "Qalculate.*"; }
+      { class = "System76 Keyboard Configurator"; }
       { class = "blueman-manager"; }
       { class = "nm-connection-editor"; }
       { class = "obs"; }
+      { class = "pavucontrol"; }
       { class = "syncthingtray"; }
       { class = "thunar"; }
-      { class = "System76 Keyboard Configurator"; }
-      { class = "pavucontrol"; }
+      { class = "zoom"; }
       { title = "Artha"; }
       { title = "Calculator"; }
+      { title = "Event Tester"; } # i.e. xev
       { title = "Steam.*"; }
       { title = "doom-capture"; }
-      { class = "zoom"; }
       { title = "kitty-floating"; } # i.e. kitty --title=kitty-floating ...
       { window_role = "pop-up"; }
       { window_role = "prefwindow"; }
-      { title = "Event Tester"; } # i.e. xev
-      { class = "Qalculate.*"; }
     ];
   };
 
