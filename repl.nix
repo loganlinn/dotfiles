beae97e53183443a0a5ef2ae57ece11d86425f52
Logan Linn <logan@llinn.dev>
2023-02-05 11:12:43 -0800

    cleaner

--- repl.nix
+++ repl.nix
@@ -1,18 +1,21 @@
 { system ? builtins.currentSystem }:
 let
   self = builtins.getFlake (toString ./.);
+
   flakeModule = self.inputs.flake-parts.lib.evalFlakeModule { inherit (self) inputs; } { };
+
   inputs' = builtins.mapAttrs (_: flakeModule.config.perInput system) self.inputs;
+
+  inputs = lib.fold lib.recursiveUpdate { } [ self.inputs inputs' ];
+
   config = flakeModule.config.perInput system self;
 
-  inherit (self.inputs.nixpkgs) lib;
-in
-self // config // {
-  inherit config lib self system;
-  inputs = lib.fold lib.recursiveUpdate { } [ self.inputs inputs' ];
-  b = builtins;
   pkgs = import self.inputs.nixpkgs {
     inherit system;
     overlays = builtins.attrValues self.overlays;
   };
-}
+
+  inherit (self.inputs.nixpkgs) lib;
+in
+
+builtins // self // { inherit config self inputs system pkgs lib; }
