c453a3fb2152d16b0e93efa5496d28eb53c7f3fc
Logan Linn <logan@llinn.dev>
2023-02-14 17:41:59 -0800

    darwin module

--- flake-modules/darwin.nix
+++ flake-modules/darwin.nix
@@ -1,3 +1,18 @@
-{ withSystem, ... }:
+{ inputs, ... }:
 
-{ }
+{
+  perSystem = ctx@{ system, pkgs, ... }: {
+    darwinConfigurations."logan@patchbook" = inputs.darwin.lib.darwinSystem {
+      inherit inputs system pkgs;
+      modules = [
+        inputs.home-manager.darwinModules.home-manager
+        ../nix/darwin/configuration.nix
+        {
+          home-manager.users.logan = import ../nix/home/darwin.nix;
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+        }
+      ];
+    };
+  };
+}
