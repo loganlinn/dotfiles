3692e4d9f61a9802385da583e60026e714fc9bf3
Logan Linn <logan@llinn.dev>
2023-02-18 00:39:47 -0800

    gh markdown

--- nix/home/dev/gh.nix
+++ nix/home/dev/gh.nix
@@ -1,61 +1,65 @@
-{lib, pkgs, ...}: {
+{ lib, pkgs, ... }: {
   programs.gh = {
     enable = true;
     enableGitCredentialHelper = true;
     settings = {
-      aliases = let
-        gh = lib.getExe pkgs.gh;
-        gum = lib.getExe pkgs.gum;
-      in {
-        o = "browse";
-        op = "pr view --web";
-        pro = "pr view --web";
-        oi = "issue list --web";
-        or = "release view --web";
-        prs = "pr list --web";
-        pco = "!${gh} prz | ifne xargs -n1 ${gh} pr checkout";
+      aliases =
+        let
+          gh = lib.getExe pkgs.gh;
+          gum = lib.getExe pkgs.gum;
+        in
+        {
+          o = "browse";
+          op = "pr view --web";
+          pro = "pr view --web";
+          oi = "issue list --web";
+          or = "release view --web";
+          prs = "pr list --web";
+          pco = "!${gh} prz | ifne xargs -n1 ${gh} pr checkout";
 
-        org-members = "api /orgs/{owner}/members --jq '.[].login'";
-        teammates = "!${gh} org-memebers | sed '/loganlinn/d'";
-        reviewers = "pr view --json 'reviewRequests' --jq '.reviewRequests[]'";
-        edit-reviewers = ''
-          !${gh} teammates |
-           ${gum} choose --selected="$(${gh} reviewers)"
-        '';
+          markdown = ''!${gh} api /markdown -f text="$(cat "''${1-/dev/stdin}")"'';
 
-        aliases = "alias list";
+          org-members = "api /orgs/{owner}/members --jq '.[].login'";
+          teammates = "!${gh} org-memebers | sed '/loganlinn/d'";
+          reviewers = "pr view --json 'reviewRequests' --jq '.reviewRequests[]'";
+          edit-reviewers = ''
+            !${gh} teammates |
+             ${gum} choose --selected="$(${gh} reviewers)"
+          '';
 
-        checks = "checks";
-        check-fail = ''
-          !${gh} pr checks "$@" | awk '$2=="fail"{ print $4 }'
-        '';
+          aliases = "alias list";
 
-        prl = ''
-          pr list
-          --json number,title,headRefName
-          --template '{{range .}}{{tablerow (printf "#%v" .number | autocolor "green") (.title | autocolor "white+h") (.headRefName | autocolor "blue")}}{{end}}'
-        '';
+          checks = "checks";
+          check-fail = ''
+            !${gh} pr checks "$@" | awk '$2=="fail"{ print $4 }'
+          '';
 
-        prz = ''
-          !${gh} prl "$@" | fzf --ansi --color  | awk '{print $1}'
-        '';
+          prl = ''
+            pr list
+            --json number,title,headRefName
+            --template '{{range .}}{{tablerow (printf "#%v" .number | autocolor "green") (.title | autocolor "white+h") (.headRefName | autocolor "blue")}}{{end}}'
+          '';
 
-        land = ''
-          !${gh} prz --author=@me | ifne xargs -n1 ${gh} pr merge --rebase --delete-branch
-        '';
+          prz = ''
+            !${gh} prl "$@" | fzf --ansi --color  | awk '{print $1}'
+          '';
 
-        landf = ''
-          !${gh} prz --author=@me | ifne xargs -n1 ${gh} pr merge --rebase --delete-branch --admin
-        '';
+          land = ''
+            !${gh} prz --author=@me | ifne xargs -n1 ${gh} pr merge --rebase --delete-branch
+          '';
 
-        gists = ''
-          !GIST=$(${gh} gist list --limit 128 | fzf -0 | cut -f1) || exit $? ; [[ -n $GIST ]] && ${gh} gist view "$GIST" "$@"
-        '';
+          landf = ''
+            !${gh} prz --author=@me | ifne xargs -n1 ${gh} pr merge --rebase --delete-branch --admin
+          '';
 
-        stars = ''
-          api user/starred --template '{{range .}}{{tablerow .full_name .description .html_url }}{{end}}'
-        '';
-      };
+          gists = ''
+            !GIST=$(${gh} gist list --limit 128 | fzf -0 | cut -f1) || exit $? ; [[ -n $GIST ]] && ${gh} gist view "$GIST" "$@"
+          '';
+
+          stars = ''
+            api user/starred --template '{{range .}}{{tablerow .full_name .description .html_url }}{{end}}'
+          '';
+        };
     };
     # extensions = [
     #   "dlvhdr/gh-dash"
