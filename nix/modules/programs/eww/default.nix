8c186a35f38d30e8e3854015a2d198923967994c
Logan Linn <logan@llinn.dev>
2023-02-21 18:00:40 -0800

    eww

--- nix/modules/programs/eww/default.nix
+++ nix/modules/programs/eww/default.nix
@@ -1,8 +1,14 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    mkIf
+    mkOption
+    mkEnableOption
+    types
+    literalExpression
+    makeBinPath
+    ;
 
   cfg = config.modules.programs.eww;
 
@@ -13,9 +19,32 @@ in
   };
 
   config = mkIf cfg.enable {
+
     programs.eww = {
       enable = true;
-      configDir = ./config;
+      configDir = ../../../../config/eww;
+    };
+
+    systemd.user.services.eww = mkIf config.programs.eww.enable {
+      Unit = {
+        Description = "Eww Daemon";
+        PartOf = [ "graphical-session.target" ];
+      };
+      Service = {
+        Environment =
+          let path = lib.makeBinPath [
+            "/run/wrappers"
+            "${config.home.homeDirectory}/.nix-profile"
+            "/etc/profiles/per-user/${config.home.username}"
+            "/nix/var/nix/profiles/default"
+            "/run/current-system/sw"
+          ];
+          in lib.mkForce "PATH=${path}";
+
+        ExecStart = "${config.programs.eww.package}/bin/eww daemon --no-daemonize";
+        Restart = "on-failure";
+      };
+      Install.WantedBy = [ "graphical-session.target" ];
     };
   };
 }
