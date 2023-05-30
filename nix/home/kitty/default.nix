ad9db47e277bdce3b1dd8d83092c988b3d2f8d09
Logan Linn <logan@llinn.dev>
2023-05-30 15:24:38 -0700

    kitty show keymap hack

--- nix/home/kitty/default.nix
+++ nix/home/kitty/default.nix
@@ -40,6 +40,7 @@
       "kitty_mod+y > l" = "kitten hints --type line --program @";
       "kitty_mod+y > w" = "kitten hints --type word --program @";
       "shift+super+w" = "close_os_window";
+      "kitty_mod+/" = ''launch --type=overlay bash -i -c 'rg "^\s*(map|mouse_map)\s+.*" ~/.config/kitty/kitty.conf | fzf' '';
     };
 
     settings = {
