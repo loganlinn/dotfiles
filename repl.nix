906f0d287dfe8586e4d198c30ffede93ab998fd1
Logan Linn <logan@llinn.dev>
2023-06-03 20:57:44 -0700

    simplify repl with flake-parts debug attrs

--- repl.nix
+++ repl.nix
@@ -1,39 +1,30 @@
-{ system ? builtins.currentSystem }:
-
 with builtins;
-
-let
-  inherit (self.inputs.flake-parts.lib) evalFlakeModule;
-  inherit (self.inputs.nixpkgs) lib;
-  inherit (lib) fold recursiveUpdate;
-
-  self = getFlake (toString ./.);
-
-  flakeModule = evalFlakeModule { inherit (self) inputs; } { };
-
-  inputs' = mapAttrs (_: flakeModule.config.perInput system) self.inputs;
-
-  inputs = lib.fold lib.recursiveUpdate { } [ self.inputs inputs' ];
-
-  config = flakeModule.config.perInput system self;
-
-  pkgs = import self.inputs.nixpkgs {
-    inherit system;
-    overlays = attrValues self.overlays;
-  };
-
-  user = getEnv "USER";
-  hostname =
+{ system ? currentSystem
+, user ? (getEnv "USER")
+, hostname ? (
     if pathExists "/etc/hostname"
-    then head (match "([a-zA-Z0-9\\-]+)\n" (readFile "/etc/hostname"))
-    else "";
+    then head (match "^([^#].*)\n$" (readFile "/etc/hostname"))
+    else (getEnv "HOSTNAME")
+  )
+}:
+let
+  # assumption: flake-parts debug is used (https://flake.parts/debug.html)
+  mkScopeAttrs = flake:
+    builtins // flake // {
+      self = flake;
+
+      inherit (flake.currentSystem.allModuleArgs)
+        config options pkgs lib;
+
+      hm = let inherit (flake.currentSystem.legacyPackages) homeConfigurations; in
+        homeConfigurations."${user}@${hostname}" or
+          homeConfigurations'.${hostname} or
+            null;
+
+      nixos = let inherit (flake) nixosConfigurations; in
+        nixosConfigurations."${user}@${hostname}" or
+          nixosConfigurations.${hostname} or
+            null;
+    };
 in
-
-builtins // self // rec {
-  inherit config self inputs system pkgs lib user hostname;
-
-  homeConfigurations = self.outputs.legacyPackages.${system}.homeConfigurations;
-
-  hm = homeConfigurations."${user}@${hostname}";
-
-}
+mkScopeAttrs (getFlake (toString ./.))
