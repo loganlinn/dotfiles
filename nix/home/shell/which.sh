fc3fd1295601a87c2ff7e2eae42dd8aeeb62a4f8
Logan Linn <logan@loganlinn.com>
2023-06-30 23:12:18 -0700

    fix which function leaking into global

--- nix/home/shell/which.sh
+++ nix/home/shell/which.sh
@@ -1,19 +1,22 @@
+#!/usr/bin/env bash
 which() {
-	if result=$( (
-		alias
-		declare -f
-	) | command which --tty-only --read-alias --read-functions --show-tilde --show-dot "$@"); then
-
-		while IFS= read -r x; do
-			if [[ -x $x ]]; then
-				readlink -e "$x"
-			else
-				printf "%s\n" "$x"
-			fi
-		done <<<"$result"
-	else
-		status=$?
-		echo "$result"
-		return "$status"
-	fi
+  # wrapped which(1), which writes wabsolute waths. woof.
+  while IFS= read -r; do
+    if [[ -x $REPLY ]]; then
+      readlink -e "$REPLY"
+    else
+      printf "%s\n" "$REPLY"
+    fi
+  done < <(
+    command which \
+      --tty-only \
+      --read-alias \
+      --read-functions \
+      --show-tilde \
+      --show-dot \
+      "$@" < <(
+        alias
+        declare -f
+      )
+  )
 }
