2a3c1609287497da7f0a996ced7d49933c76fb94
Logan Linn <logan@loganlinn.com>
2023-07-08 17:10:31 -0700

    fix bash module

--- nix/home/bash/default.nix
+++ nix/home/bash/default.nix
@@ -1,5 +1,12 @@
 { config, lib, pkgs, ... }:
 
 {
+  home.packages = with pkgs; [
+    bashInteractive # otherwise we get bashNonInteractive (?)
+  ];
 
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+  };
 }
