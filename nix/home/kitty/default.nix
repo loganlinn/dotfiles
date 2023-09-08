11d9023c63b3bcfb9722b505dade421cf7f5317f
Logan Linn <logan@loganlinn.com>
2023-09-08 09:48:55 -0700

    os-window for pr check hud

--- nix/home/kitty/default.nix
+++ nix/home/kitty/default.nix
@@ -55,7 +55,7 @@ in
     "kitty_mod+y>l" = "kitten hints --type line --program @";
     "kitty_mod+y>w" = "kitten hints --type word --program @";
     "shift+super+w" = "close_os_window";
-    "kitty_mod+m>p" =  ''launch --location=neighbor --cwd=current bash -c 'gh pr checks --watch && read -n 1 -s -r -p "Press any key to exit"' '';
+    "kitty_mod+m>p" =  ''launch --type=os-window --cwd=current bash -c 'gh pr checks --watch && read -n 1 -s -r -p "Press any key to exit"' '';
     "kitty_mod+/" = ''launch --type=overlay bash -i -c 'rg "^\s*(map|mouse_map)\s+.*" ~/.config/kitty/kitty.conf | fzf' '';
     # git stash show
     "kitty_mod+o>s" = ''kitten hints --type regex --regex '(?m)(stash@\{[^}]+\})' --program 'launch --type=overlay git stash show -p' '';
