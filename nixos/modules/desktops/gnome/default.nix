fa4416175141cc3b1bdd2186f275a55de00d131b
Logan Linn <logan@loganlinn.com>
2023-09-09 17:14:22 -0700

    modules/desktops/{gnome,hyperland}

---
+++ nixos/modules/desktops/gnome/default.nix
@@ -0,0 +1,9 @@
+{ config, lib, pkgs, ... }:
+
+{
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
+}
---
+++ nixos/modules/desktops/hyprland/default.nix
@@ -0,0 +1,5 @@
+{ pkgs, ... }: {
+  programs.hyprland.enable = true;
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+}
