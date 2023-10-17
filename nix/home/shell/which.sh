c7c35c41269410937ee5c9344dea136261102db5
Logan Linn <logan@loganlinn.com>
2023-10-17 00:20:02 -0700

    disable which wrapper

--- nix/home/shell/which.sh
+++ nix/home/shell/which.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
-which() {
-  # wrapped which(1), which writes wabsolute waths. woof.
-  while IFS= read -r; do
-    if [[ -x $REPLY ]]; then
-      readlink -e "$REPLY"
-    else
-      printf "%s\n" "$REPLY"
-    fi
-  done < <(
-    command which \
-      --tty-only \
-      --read-alias \
-      --read-functions \
-      --show-tilde \
-      --show-dot \
-      "$@" < <(
-        alias
-        declare -f
-      )
-  )
-}
+# which() {
+#   # wrapped which(1), which writes wabsolute waths. woof.
+#   while IFS= read -r; do
+#     if [[ -x $REPLY ]]; then
+#       readlink -e "$REPLY"
+#     else
+#       printf "%s\n" "$REPLY"
+#     fi
+#   done < <(
+#     command which \
+#       --tty-only \
+#       --read-alias \
+#       --read-functions \
+#       --show-tilde \
+#       --show-dot \
+#       "$@" < <(
+#         alias
+#         declare -f
+#       )
+#   )
+# }
