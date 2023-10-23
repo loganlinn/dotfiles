692f1d08b070a4218c6b36779779d68a6e00fc2b
Logan Linn <logan@loganlinn.com>
2023-10-23 16:33:41 -0700

    touchpad defaults

--- nixos/modules/xserver.nix
+++ nixos/modules/xserver.nix
@@ -1,15 +1,28 @@
-{ config, lib, pkgs, ... }:
-
-with lib;
-
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; {
   config = mkIf config.services.xserver.enable {
     console.useXkbConfig = true;
-    services.xserver.autorun = mkDefault true;
-    services.xserver.layout = "us";
-    services.xserver.xkbOptions = "ctrl:nocaps"; # Make Caps Lock a Control key
-
-    #  xkb directory (and the xorg.conf file) gets exported to /etc/X11/xkb, which is useful if you have to often look stuff up in it.
-    services.xserver.exportConfiguration = mkDefault true;
+    services.xserver = {
+      autorun = mkDefault true;
+      layout = "us";
+      xkbOptions = "ctrl:nocaps"; # Make Caps Lock a Control key
+      #  xkb directory (and the xorg.conf file) gets exported to /etc/X11/xkb, which is useful if you have to often look stuff up in it.
+      exportConfiguration = mkDefault true;
+      libinput.touchpad = mkDefault {
+        accelProfile = "adaptive";
+        clickMethod = "none";
+        disableWhileTyping = true;
+        horizontalScrolling = false;
+        naturalScrolling = true;
+        scrollMethod = "twofinger";
+        tapping = false;
+        tappingDragLock = true;
+      };
+    };
   };
 }
