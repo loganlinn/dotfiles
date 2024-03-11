c991fa97ee4172857cfff5a1e9008c9b449a8c7f
Logan Linn <logan@loganlinn.com>
2024-03-10 18:17:20 -0700

    wijusan updates

--- home-manager/wijusan.nix
+++ home-manager/wijusan.nix
@@ -13,6 +13,7 @@
   ];
 
   colorScheme = nix-colors.colorSchemes.doom-one; # needed?
+
   # my.java.package = pkgs.jdk17;
   # my.java.toolchains = with pkgs; [ jdk8 jdk11 ];
   modules.spellcheck.enable = true;
@@ -25,11 +26,18 @@
     gpg.ssh.program = "/mnt/c/Users/logan/AppData/Local/1Password/app/8/op-ssh-sign-wsl";
     commit.gpgsign = true;
   };
+
   programs.emacs.enable = true;
   programs.emacs.package = pkgs.emacs-pgtk; # native Wayland support
+  services.emacs.enable = true;
+
   # programs.nix-index.enable = false;
   # programs.nix-index.enableZshIntegration = config.programs.nix-index.enable;
 
+  home.packages = with pkgs; [
+    wslu
+    trashy
+  ];
   home.username = "logan";
   home.homeDirectory = "/home/logan";
   home.stateVersion = "22.11";
