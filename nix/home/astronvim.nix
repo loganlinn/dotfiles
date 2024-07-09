01b740b9e58d405d3792a3e88f06552c78ac88fd
Logan Linn <logan@loganlinn.com>
2024-07-09 15:55:58 -0700

    use 'run' helper provided by home.activation

--- nix/home/astronvim.nix
+++ nix/home/astronvim.nix
@@ -1,15 +1,7 @@
-{ config, pkgs, lib, ... }:
-
+{ config, lib, ... }:
 with lib;
-
-
 let
-  inherit (lib.my) toExe;
-
   cfg = config.my.astronvim;
-
-  git = toExe config.programs.git;
-
 in
 {
   options.my.astronvim = {
@@ -30,22 +22,27 @@ in
   };
 
   config = mkIf cfg.enable {
-    home.activation.astrovim = mkIf (cfg.configRepo != null) (lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      if ! [[ -d ${config.xdg.configHome}/nvim ]]; then
-        $DRY_RUN_CMD ${git} clone $VERBOSE_ARG --depth 1 "${cfg.configRepo}" "${config.xdg.configHome}/nvim"
-        if [[ -d ${config.xdg.dataHome}/nvim ]]; then
-          $DRY_RUN_CMD mv $VERBOSE_ARG "${config.xdg.dataHome}/nvim" "${config.xdg.dataHome}/nvim~"
-        fi
-        if [[ -d ${config.xdg.stateHome}/nvim ]]; then
-          $DRY_RUN_CMD mv $VERBOSE_ARG "${config.xdg.stateHome}/nvim" "${config.xdg.stateHome}/nvim~"
-        fi
-      fi
-    '');
 
-    home.activation.astrovimUser = mkIf (cfg.userConfigRepo != null) (lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      if ! [[ -d ${config.xdg.configHome}/astronvim ]]; then
-        $DRY_RUN_CMD ${git} clone $VERBOSE_ARG --depth 1 "${cfg.userConfigRepo}" "${config.xdg.configHome}/astronvim/lua/user"
-      fi
-    '');
+    home.activation.astronvim = mkIf (cfg.configRepo != null) (
+      lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        if ! [[ -d ${config.xdg.configHome}/nvim ]]; then
+          run git clone $VERBOSE_ARG --depth 1 "${cfg.configRepo}" "${config.xdg.configHome}/nvim"
+          if [[ -d ${config.xdg.dataHome}/nvim ]]; then
+            run mv $VERBOSE_ARG "${config.xdg.dataHome}/nvim" "${config.xdg.dataHome}/nvim~"
+          fi
+          if [[ -d ${config.xdg.stateHome}/nvim ]]; then
+            run mv $VERBOSE_ARG "${config.xdg.stateHome}/nvim" "${config.xdg.stateHome}/nvim~"
+          fi
+        fi
+      ''
+    );
+
+    home.activation.astronvimUser = mkIf (cfg.userConfigRepo != null) (
+      lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        if ! [[ -d ${config.xdg.configHome}/astronvim ]]; then
+          run git clone $VERBOSE_ARG --depth 1 "${cfg.userConfigRepo}" "${config.xdg.configHome}/astronvim/lua/user"
+        fi
+      ''
+    );
   };
 }
