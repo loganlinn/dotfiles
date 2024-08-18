2620aa14e9d4407f2e190bd5366a7cc0446cfee9
Logan Linn <logan@loganlinn.com>
2024-08-18 01:47:36 -0700

    update flake, add git-repo-manager flake

--- flake.nix
+++ flake.nix
@@ -6,34 +6,78 @@
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.05";
 
     ## builders
-    home-manager = {url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs";};
-    nix-darwin = {url = "github:lnl7/nix-darwin"; inputs.nixpkgs.follows = "nixpkgs";};
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixos-hardware.url = "github:nixos/nixos-hardware";
-    nixos-wsl = {url = "github:nix-community/NixOS-WSL"; inputs.nixpkgs.follows = "nixpkgs";};
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     ## packages
-    eww = {url = "github:elkowar/eww"; inputs.nixpkgs.follows = "nixpkgs";};
+    eww = {
+      url = "github:elkowar/eww";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     hyprland.url = "github:hyprwm/Hyprland";
-    # hyprland-plugins = {url = "github:hyprwm/hyprland-plugins"; inputs.hyprland.follows = "hyprland";};
-
-    ## overlays
-    emacs-overlay = {url = "github:nix-community/emacs-overlay"; inputs.nixpkgs.follows = "nixpkgs"; inputs.nixpkgs-stable.follows = "nixpkgs-stable";};
-    fenix = {url = "github:nix-community/fenix"; inputs.nixpkgs.follows = "nixpkgs";};
-    rust-overlay = {url = "github:oxalica/rust-overlay"; inputs.nixpkgs.follows = "nixpkgs";};
+    git-repo-manager = {
+      url = "github:hakoerber/git-repo-manager";
+    };
+    emacs-overlay = {
+      url = "github:nix-community/emacs-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-stable.follows = "nixpkgs-stable";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     ## utils
-    agenix = {url = "github:ryantm/agenix"; inputs.nixpkgs.follows = "nixpkgs";};
-    devenv = {url = "github:cachix/devenv"; inputs.nixpkgs.follows = "nixpkgs";};
-    flake-compat = {url = "github:edolstra/flake-compat"; flake = false;};
-    flake-parts = {url = "github:hercules-ci/flake-parts"; inputs.nixpkgs-lib.follows = "nixpkgs";};
-    flake-root = { url = "github:srid/flake-root"; };
-    mission-control = { url = "github:Platonic-Systems/mission-control"; };
-    nix-colors = { url = "github:misterio77/nix-colors"; };
-    nix-index-database = {url = "github:Mic92/nix-index-database"; inputs.nixpkgs.follows = "nixpkgs";};
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    flake-root = {
+      url = "github:srid/flake-root";
+    };
+    mission-control = {
+      url = "github:Platonic-Systems/mission-control";
+    };
+    nix-colors = {
+      url = "github:misterio77/nix-colors";
+    };
+    nix-index-database = {
+      url = "github:Mic92/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixpkgs-match.url = "github:srid/nixpkgs-match";
   };
 
-  outputs = inputs@{ self, ... }:
+  outputs =
+    inputs@{ self, ... }:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         inputs.flake-parts.flakeModules.easyOverlay
@@ -43,134 +87,152 @@
         ./flake-module.nix
       ];
 
-      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
 
-      perSystem = ctx@{ inputs', self', config, system, pkgs, lib, ... }: {
-        imports = [ ./options.nix ];
+      perSystem =
+        ctx@{
+          inputs',
+          self',
+          config,
+          system,
+          pkgs,
+          lib,
+          ...
+        }:
+        {
+          imports = [ ./options.nix ];
 
-        _module.args.pkgs = import inputs.nixpkgs {
-          inherit system;
-          config = import ./config/nixpkgs/config.nix;
-          overlays = [
-            self.overlays.default
-            inputs.emacs-overlay.overlays.default
-            inputs.fenix.overlays.default
-          ];
-        };
-
-        packages = import ./nix/pkgs { inherit pkgs; };
-
-        apps.default = {
-          type = "app";
-          program = "${pkgs.cowsay}/bin/cowsay";
-        };
-
-        apps.nixpkgs-match = {
-          type = "app";
-          program = ''nix run github:srid/nixpkgs-match -- "$@"'';
-        };
-
-        overlayAttrs = {
-          inherit (inputs'.home-manager.packages) home-manager;
-          inherit (inputs'.devenv.packages) devenv;
-          inherit (inputs'.emacs.packages) emacs-unstable;
-          inherit (inputs'.agenix.packages) agenix;
-        };
-
-        formatter = pkgs.nixpkgs-fmt;
-
-        devShells = {
-          default = pkgs.mkShell {
-            inputsFrom = [
-              config.flake-root.devShell # sets FLAKE_ROOT
-              config.mission-control.devShell
-            ];
-            buildInputs = [ config.formatter inputs'.agenix.packages.agenix ];
-            env.NIX_USER_CONF_FILES = toString ./nix.conf;
-          };
-        } // (
-          lib.listToAttrs (
-            lib.forEach [
-              "stable"
-              "beta"
-              "minimal"
-              "default"
-              "complete"
-              "latest"
-            ]
-              (toolchain: {
-                name = "rust-${toolchain}";
-                value = pkgs.mkShell {
-                  buildInputs = with pkgs; [
-                    cmake
-                    llvmPackages.bintools
-                    openssl
-                    pkg-config
-                    # (rust-bin.stable.latest.default.override {
-                    #   targets = [ "wasm32-unknown-unknown" ];
-                    # })
-                    (fenix."${toolchain}".withComponents [
-                      # https://rust-lang.github.io/rustup/concepts/components.html
-                      "cargo"
-                      "clippy"
-                      "rust-docs"
-                      "rust-src"
-                      "rustc"
-                      "rustfmt"
-                    ])
-                    rust-analyzer-nightly
-                  ];
-                };
-              })
-          ));
-
-        legacyPackages = lib.optionalAttrs (ctx.system == "x86_64-linux") {
-          homeConfigurations = {
-            "logan@nijusan" = self.lib.dotfiles.mkHomeConfiguration ctx [
-              self.homeModules.common
-              self.homeModules.nix-colors
-              self.homeModules.secrets
-              ./home-manager/nijusan.nix
-            ];
-            "logan@wijusan" = self.lib.dotfiles.mkHomeConfiguration ctx [
-              self.homeModules.common
-              self.homeModules.nix-colors
-              self.homeModules.secrets
-              ./home-manager/wijusan.nix
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            config = import ./config/nixpkgs/config.nix;
+            overlays = [
+              self.overlays.default
+              # TODO: use overlay conditionally, i.e. https://github.com/hlissner/dotfiles/blob/1e2ca74b02d2d92005352bf328acc86abb10efbd/modules/editors/emacs.nix#L28-L31
+              inputs.emacs-overlay.overlays.default
+              inputs.git-repo-manager.overlays.git-repo-manager
+              inputs.fenix.overlays.default
             ];
           };
+
+          packages = import ./nix/pkgs { inherit pkgs; };
+
+          apps.default = {
+            type = "app";
+            program = "${pkgs.cowsay}/bin/cowsay";
+          };
+
+          apps.nixpkgs-match = {
+            type = "app";
+            program = ''nix run github:srid/nixpkgs-match -- "$@"'';
+          };
+
+          overlayAttrs = {
+            inherit (inputs'.home-manager.packages) home-manager;
+            inherit (inputs'.devenv.packages) devenv;
+            inherit (inputs'.emacs.packages) emacs-unstable;
+            inherit (inputs'.agenix.packages) agenix;
+          };
+
+          formatter = pkgs.nixpkgs-fmt;
+
+          devShells =
+            {
+              default = pkgs.mkShell {
+                inputsFrom = [
+                  config.flake-root.devShell # sets FLAKE_ROOT
+                  config.mission-control.devShell
+                ];
+                buildInputs = [
+                  config.formatter
+                  inputs'.agenix.packages.agenix
+                ];
+                env.NIX_USER_CONF_FILES = toString ./nix.conf;
+              };
+            }
+            // (lib.listToAttrs (
+              lib.forEach
+                [
+                  "stable"
+                  "beta"
+                  "minimal"
+                  "default"
+                  "complete"
+                  "latest"
+                ]
+                (toolchain: {
+                  name = "rust-${toolchain}";
+                  value = pkgs.mkShell {
+                    buildInputs = with pkgs; [
+                      cmake
+                      llvmPackages.bintools
+                      openssl
+                      pkg-config
+                      # (rust-bin.stable.latest.default.override {
+                      #   targets = [ "wasm32-unknown-unknown" ];
+                      # })
+                      (fenix."${toolchain}".withComponents [
+                        # https://rust-lang.github.io/rustup/concepts/components.html
+                        "cargo"
+                        "clippy"
+                        "rust-docs"
+                        "rust-src"
+                        "rustc"
+                        "rustfmt"
+                      ])
+                      rust-analyzer-nightly
+                    ];
+                  };
+                })
+            ));
+
+          legacyPackages = lib.optionalAttrs (ctx.system == "x86_64-linux") {
+            homeConfigurations = {
+              "logan@nijusan" = self.lib.dotfiles.mkHomeConfiguration ctx [
+                self.homeModules.common
+                self.homeModules.nix-colors
+                self.homeModules.secrets
+                ./home-manager/nijusan.nix
+              ];
+              "logan@wijusan" = self.lib.dotfiles.mkHomeConfiguration ctx [
+                self.homeModules.common
+                self.homeModules.nix-colors
+                self.homeModules.secrets
+                ./home-manager/wijusan.nix
+              ];
+            };
+          };
         };
-      };
 
       flake = {
-        nixosConfigurations.nijusan =
-          self.lib.dotfiles.mkNixosSystem "x86_64-linux" [
-            ./nixos/nijusan/configuration.nix
-            # self.nixosModules.home-manager
-          ];
+        nixosConfigurations.nijusan = self.lib.dotfiles.mkNixosSystem "x86_64-linux" [
+          ./nixos/nijusan/configuration.nix
+          # self.nixosModules.home-manager
+        ];
 
-        nixosConfigurations.framework =
-          self.lib.dotfiles.mkNixosSystem "x86_64-linux" [
-            ./nixos/framework/configuration.nix
-            inputs.agenix.nixosModules.default
-            self.nixosModules.home-manager
-            {
-              home-manager.users.logan = import ./nixos/framework/home.nix; # TODO unify with nijusan
-            }
-          ];
+        nixosConfigurations.framework = self.lib.dotfiles.mkNixosSystem "x86_64-linux" [
+          ./nixos/framework/configuration.nix
+          inputs.agenix.nixosModules.default
+          self.nixosModules.home-manager
+          {
+            home-manager.users.logan = import ./nixos/framework/home.nix; # TODO unify with nijusan
+          }
+        ];
 
-        nixosConfigurations.wijusan =
-          self.lib.dotfiles.mkNixosSystem "x86_64-linux" [
-            ./nixos/wijusan/configuration.nix
-          ];
+        nixosConfigurations.wijusan = self.lib.dotfiles.mkNixosSystem "x86_64-linux" [
+          ./nixos/wijusan/configuration.nix
+        ];
 
-        darwinConfigurations.patchbook =
-          self.lib.dotfiles.mkDarwinSystem "aarch64-darwin" [
-            self.darwinModules.common
-            self.darwinModules.home-manager
-            ./nix-darwin/patchbook.nix
-            {
-              home-manager.users.logan = { options, config, ... }: {
+        darwinConfigurations.patchbook = self.lib.dotfiles.mkDarwinSystem "aarch64-darwin" [
+          self.darwinModules.common
+          self.darwinModules.home-manager
+          ./nix-darwin/patchbook.nix
+          {
+            home-manager.users.logan =
+              { options, config, ... }:
+              {
                 imports = [
                   self.homeModules.common
                   self.homeModules.nix-colors
@@ -179,8 +241,8 @@
                 ];
                 home.stateVersion = "22.11";
               };
-            }
-          ];
+          }
+        ];
       };
 
       # my repl depends on attrs exposed by flake-parts's debug option.
