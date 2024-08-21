cb22e1c33836e8cf7004c4efb03600f505c1327f
Logan Linn <logan@loganlinn.com>
2024-08-20 19:54:23 -0700

    nix-registry: add dotfiles to registry + general rewrite

--- nix/modules/nix-registry.nix
+++ nix/modules/nix-registry.nix
@@ -1,26 +1,35 @@
-{ inputs, config, lib, ... }:
+# [`nix registry`](https://nix.dev/manual/nix/2.24/command-ref/new-cli/nix3-registry)
+{
+  self,
+  inputs,
+  config,
+  lib,
+  ...
+}:
 
 with lib;
 
-let
-
-  cfg = config.my.nix-registry;
-
-in
+let cfg = config.my.nix-registry; in
 {
   options.my.nix-registry = {
     fromInputs = mkOption {
       type = types.listOf types.str;
-      default = [ "nixpkgs" "home-manager" ];
+      default = [
+        "nixpkgs"
+        "home-manager"
+      ];
     };
   };
 
   config = {
-    nix.registry = genAttrs cfg.fromInputs (name: {
-      to = {
-        type = "path";
-        path = inputs."${name}";
-      };
-    });
+    nix.registry =
+      let
+        inputFlakes = (getAttrs cfg.fromInputs inputs);
+        extraFlakes = {
+          dotfiles = self;
+        };
+        allFlakes = attrsets.unionOfDisjoint inputFlakes extraFlakes;
+      in
+        mapAttrs (name: flake: { inherit flake; }) allFlakes;
   };
 }
