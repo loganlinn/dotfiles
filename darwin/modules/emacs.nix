d752da2ebc03bbae6f8a04f9d817b7e360769835
Logan Linn <logan@loganlinn.com>
2024-10-10 01:26:03 -0700

    darwin/emacs: with xwidgets

--- darwin/modules/emacs.nix
+++ darwin/modules/emacs.nix
@@ -11,7 +11,7 @@
         "with-native-comp" # build with native compilation aka gccemacs
         # "with-debug"     # build with debug symbols and debugger friendly optimizations
         # "with-poll"      # build with poll() instead of select() to enable more file descriptors
-        # "with-xwidgets"  # build with native macos webkit support
+        "with-xwidgets"  # build with native macos webkit support
         # "with-no-frame-refocus" # disables frame re-focus (ie. closing one frame does not refocus another one)
       ];
     }
