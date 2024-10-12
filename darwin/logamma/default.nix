0629c5c6760ab32af8feee383fccf60db4462f95
Logan Linn <logan@loganlinn.com>
2024-10-12 02:59:00 -0700

    darwin: atlas completions (tryhard)

--- darwin/logamma/default.nix
+++ darwin/logamma/default.nix
@@ -1,6 +1,7 @@
 {
   self,
   inputs,
+  config,
   pkgs,
   lib,
   ...
@@ -22,6 +23,20 @@
     {
       homebrew.taps = [ "ariga/tap" ];
       homebrew.brews = [ "ariga/tap/atlas" ];
+      environment.systemPackages = [
+        (pkgs.stdenv.mkDerivation {
+          pname = "atlas-shell-completion";
+          version = "0.0.1";
+          dontUnpack = true; # no src
+          nativeBuildInputs = [ pkgs.installShellFiles ];
+          postInstall = ''
+            installShellCompletion --cmd atlas \
+            --bash <(${config.homebrew.brewPrefix}/atlas completion bash) \
+            --fish <(${config.homebrew.brewPrefix}/atlas completion fish) \
+            --zsh <(${config.homebrew.brewPrefix}/atlas completion zsh)
+          '';
+        })
+      ];
     }
   ];
 
