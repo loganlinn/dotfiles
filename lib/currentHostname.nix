27577d48d64869be156bc87d33e13704b3a6ef6b
Logan Linn <logan@loganlinn.com>
2024-10-16 14:52:44 -0700

    support hostname lookup on macos

--- lib/currentHostname.nix
+++ lib/currentHostname.nix
@@ -1,7 +1,37 @@
 with builtins;
-
-head (match "([a-zA-Z0-9]+)\n" (
-  if pathExists "/etc/hostname"
-  then readFile "/etc/hostname"
-  else getEnv "HOST"
-))
+let
+  pkgs = import <nixpkgs> { };
+  when = c: x: if c then x else null;
+  some =
+    xs:
+    let
+      xs' = filter (x: x != null) xs;
+    in
+    when (xs' != [ ]) (head xs');
+in
+with pkgs.lib;
+some [
+  # macOS stores hostname in plist, of course.
+  (when pkgs.stdenv.isDarwin
+    # poorman's xml extraction
+    (
+      pipe "/Library/Preferences/SystemConfiguration/preferences.plist" [
+        readFile
+        (split "<key>LocalHostName</key>")
+        last
+        trim
+        (match "^<string>([^<]*)</string>.+")
+        head
+      ]
+    )
+  )
+  (when (pathExists "/etc/hostname") (
+    pipe "/etc/hostname" [
+      readFile
+      trim
+      (match "([a-zA-Z0-9]+)")
+      head
+    ]
+  ))
+  (getEnv "HOST")
+]
