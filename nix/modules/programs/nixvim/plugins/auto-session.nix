4eddc0e326a54417110788652755a4bb5c3273e8
Logan Linn <logan@loganlinn.com>
2025-01-10 22:53:52 -0800

    nixvim: session keys

--- nix/modules/programs/nixvim/plugins/auto-session.nix
+++ nix/modules/programs/nixvim/plugins/auto-session.nix
@@ -7,23 +7,52 @@ in
     plugins.auto-session = {
       enable = true;
       settings = {
+        lazyLoad = true;
         keys = [
           {
             mode = "n";
             key = "<leader>qs";
             action = "<cmd>SessionSave<cr>";
-            options.desc = "Session save";
-            options.silent = true;
+            options.desc = "Save session";
           }
           {
             mode = "n";
             key = "<leader>qd";
-            action = "<cmd>SessionSave<cr>";
-            options.desc = "Session delete";
-            options.silent = true;
+            action = "<cmd>SessionDelete<cr>";
+            options.desc = "Delete session";
+          }
+          {
+            mode = "n";
+            key = "<leader>qD";
+            action = "<cmd>Autosession delete<cr>";
+            options.desc = "Delete a session";
+          }
+          {
+            mode = "n";
+            key = "<leader>qP";
+            action = "<cmd>SessionPurgeOrphaned<cr>";
+            options.desc = "Purge orphaned sessions";
+          }
+          {
+            mode = "n";
+            key = "<leader>qL";
+            action = "<cmd>Autosession search<cr>";
+            options.desc = "Restore a session";
+          }
+          {
+            mode = "n";
+            key = "<leader>ql";
+            action = "<cmd>SessionRestore<cr>";
+            options.desc = "Restore session";
+          }
+          {
+            mode = "n";
+            key = "<leader>tS";
+            action = "<cmd>SessionToggleAutoSave<cr>";
+            options.desc = "Toggle session auto-save";
           }
         ];
-
+        show_auto_restore_notif = true;
         pre_save_cmds.__raw = ''
           {
             require("nvim-tree.api").tree.close
