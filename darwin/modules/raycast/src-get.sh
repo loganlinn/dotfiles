8af4f6e38099cb4334896dc799c24d7b4532efd5
Logan Linn <logan@loganlinn.com>
2025-05-02 13:15:58 -0700

    raycast: src-get quick command in terminal

--- darwin/modules/raycast/src-get.sh
+++ darwin/modules/raycast/src-get.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 # @raycast.schemaVersion 1
 # @raycast.title src-get
-# @raycast.mode fullOutput
+# @raycast.mode silent
 # @raycast.icon 🧑‍💻
 # @raycast.argument1 { "type": "text", "placeholder": "repository" }
 
 # Documentation:
 # @raycast.author loganlinn
 # @raycast.authorURL https://github.com/loganlinn
-
-export PATH="/etc/profiles/per-user/$USER/bin:$PATH"
 
 #shellcheck disable=SC1090
 source-if-exists() {
@@ -21,43 +19,12 @@ source-if-exists() {
   done
 }
 
+export PATH="$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:$PATH"
+
 source-if-exists \
   "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" \
-  "/etc/profiles/per-user/logan/etc/profile.d/hm-session-vars.sh"
+  "/etc/profiles/per-user/$USER/etc/profile.d/hm-session-vars.sh"
 
-case $1 in
-# i.e. https://github.com/loganlinn/dotfiles
-*://*) repo=$1 ;;
-# i.e. git@github.com:loganlinn/dotfiles
-*@*:*) repo=$1 ;;
-# i.e. github.com/loganlinn/dotfiles
-*.*/*) repo="https://$1" ;;
-# i.e. loganlinn/dotfiles
-*/*) repo="https://github.com/$1" ;;
-*)
-  echo "Invalid repository reference '$1'" >&2
-  exit 1
-  ;;
-esac
-
-case "$1" in
-*@*:*)
-  dir="${1#*@}"
-  dir=${dir/:/\/}
-  ;;
-*)
-  dir="${1#*://}"
-  ;;
-esac
-dir="${dir%%.git}"
-dir=${SRC_HOME:=$HOME/src}/$dir
-
-set -x
-
-if ! [[ -d "$dir" ]]; then
-  git clone --depth=1 --progress -- "$repo" "$dir" || exit 1
+if PANE_ID=$(wezterm cli spawn); then
+  wezterm cli send-text --pane-id "$PANE_ID" --no-paste "src-get $(printf %q "$1")"$'\n'
 fi
-
-zoxide add "$dir" || true
-
-open "$dir"
