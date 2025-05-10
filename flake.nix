773ac28df335e1254c85ba609a0131da105209b1
Logan Linn <logan@loganlinn.com>
2025-05-09 22:33:01 -0700

    nixpkgs-stable 24.11

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
     determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/0.1";
     ## builders
     home-manager.url = "github:nix-community/home-manager";
