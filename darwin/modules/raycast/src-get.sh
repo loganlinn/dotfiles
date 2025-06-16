0b8ef03bc0dbd08f5b5ec10f1fa34bca076d3139
Logan Linn <logan@loganlinn.com>
2025-06-15 23:12:28 -0700

    fix src-get raycast script command

--- darwin/modules/raycast/src-get.sh
+++ darwin/modules/raycast/src-get.sh
@@ -3,11 +3,15 @@
 # @raycast.title src-get
 # @raycast.mode silent
 # @raycast.icon 🧑‍💻
-# @raycast.argument1 { "type": "text", "placeholder": "repository" }
+# @raycast.argument1 { "type": "text", "placeholder": "rep (optional)", "optional": true }
 
 # Documentation:
 # @raycast.author loganlinn
 # @raycast.authorURL https://github.com/loganlinn
+
+set -e
+
+export PATH="$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:$PATH"
 
 #shellcheck disable=SC1090
 source-if-exists() {
@@ -19,12 +23,14 @@ source-if-exists() {
   done
 }
 
-export PATH="$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:$PATH"
-
 source-if-exists \
   "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" \
   "/etc/profiles/per-user/$USER/etc/profile.d/hm-session-vars.sh"
 
-if PANE_ID=$(wezterm cli spawn); then
-  wezterm cli send-text --pane-id "$PANE_ID" --no-paste "src-get $(printf %q "$1")"$'\n'
+if [[ -z "$repo" ]]; then
+  repo=$(pbpaste)
 fi
+
+NL=$'\n'
+
+wezterm cli send-text --pane-id "$(wezterm cli spawn)" --no-paste "$(printf 'src-get %q' "$repo")$NL"
