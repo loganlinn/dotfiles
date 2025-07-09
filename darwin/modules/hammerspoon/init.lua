3acbdecfce7bc04607d24b468b73798e5a459757
Logan Linn <logan@loganlinn.com>
2025-07-08 22:49:50 -0700

    hamnmerspoon: sync

--- darwin/modules/hammerspoon/init.lua
+++ darwin/modules/hammerspoon/init.lua
@@ -1,76 +1,74 @@
-require("hs.ipc") -- enables `hc` CLI
-
+local hs = _G["hs"]
+local _ = require("hs.ipc") -- enables `hc` CLI
+local alert = require("hs.alert")
 local logger = require("hs.logger")
-
+local hotkey = require("hs.hotkey")
 local log = logger.new("hammerspoon.init", "info")
 
-local pathlib = {}
+----------------------------------------
+-- Helpers
 
---- Expands tilde.
----@param path string
----@return string
-function pathlib.expand(path)
-  if path:sub(1, 1) == "~" then
-    path = os.getenv("HOME") .. path:sub(2)
-  end
-  return path
-end
-
-function pathlib.join(dir, base)
-  if dir:sub(-1) ~= "/" and base:sub(1, 1) ~= "/" then
-    dir = dir .. "/"
-  end
-  return dir .. base
-end
-
----@param iter string[]|function
----param name string
----@return string|nil
-function pathlib.search(iter, name)
-  if type(iter) ~= "function" then
-    iter = ipairs(iter)
-  end
-  for _, path in iter do
-    local pathname = pathlib.join(path, name)
-    if hs.fs.attributes(pathname, "inode") then
-      return pathname
+local partial = function(f, ...)
+  local f1 = function(f, x)
+    return function(...)
+      return f(x, ...)
     end
   end
-end
-
-function pathlib.findNixApp(name)
-  return pathlib.search({
-    "/Applications/Nix Apps",
-    "/Applications/Nix Apps",
-    "/Applications/Home Manager Apps",
-  }, name .. ".app")
-end
-
-local function launchOrFocusFn(app)
-  return function()
-    hs.application.launchOrFocus(app)
+  for i = 1, select("#", ...) do
+    f = f1(f, select(i, ...))
   end
+  return f
 end
 
-local function activateWezterm()
-  log.d("activateWezterm")
-  local app = hs.application.get("com.github.wez.wezterm")
-  if app then
-    return app:activate()
-  end
-  local path = pathlib.findNixApp("WezTerm")
-  if path then
-    return hs.application.launchOrFocus(path)
-  end
+local execute = function(...)
+  log.i("[execute]", ...)
+  return hs.execute(...)
 end
 
-hs.hotkey.bind("⌥", "return", activateWezterm)
-hs.hotkey.bind("⌃⌥", "r", hs.reload)
-hs.hotkey.bind("⇧⌥", "return", launchOrFocusFn("Google Chrome"))
-hs.hotkey.bind("⌥", "e", launchOrFocusFn("Emacs"))
-hs.hotkey.bind("⌥", "m", launchOrFocusFn("Messages"))
-hs.hotkey.bind("⌥", "o", launchOrFocusFn("Finder"))
-hs.hotkey.bind("⌥", "p", launchOrFocusFn("Claude"))
-hs.hotkey.bind("⌥", "s", launchOrFocusFn("Slack"))
+local wezterm = {}
 
-hs.alert.show("✅ Hammerspoon")
+wezterm.application = hs.application.find("com.github.wez.wezterm")
+  or hs.application.find("WezTerm")
+  or log.w("WezTerm not found")
+  or nil
+
+wezterm.activate = function()
+  return wezterm.application and wezterm.application:activate()
+end
+
+local aerospace = {}
+
+aerospace.reload = function()
+  execute("aerospace reload-config", true)
+  alert("✅ Aerospace")
+end
+
+----------------------------------------
+-- Hotkeys
+
+local CTRL = "⌃"
+local ALT = "⌥"
+local SHIFT = "⇧"
+local GUI = "⌘"
+local MEH = CTRL .. SHIFT .. ALT
+local HYPER = CTRL .. SHIFT .. ALT .. GUI
+
+local launchOrFocusFn = partial(partial, hs.application.launchOrFocus)
+
+local function lockScreen()
+  hs.caffeinate.lockScreen()
+end
+
+hs.hotkey.bind(ALT, "return", wezterm.activate)
+hs.hotkey.bind(SHIFT .. ALT, "return", launchOrFocusFn("Google Chrome"))
+hs.hotkey.bind(ALT, "e", launchOrFocusFn("Emacs"))
+hs.hotkey.bind(ALT, "m", launchOrFocusFn("Messages"))
+hs.hotkey.bind(ALT, "o", launchOrFocusFn("Finder"))
+hs.hotkey.bind(ALT, "p", launchOrFocusFn("Claude"))
+hs.hotkey.bind(ALT, "s", launchOrFocusFn("Slack"))
+hs.hotkey.bind(HYPER, "`", hs.toggleConsole)
+hs.hotkey.bind(HYPER, "a", aerospace.reload)
+hs.hotkey.bind(HYPER, "l", lockScreen)
+hs.hotkey.bind(HYPER, "r", hs.reload)
+
+alert("✅ Hammerspoon")
