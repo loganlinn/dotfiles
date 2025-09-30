cb386b1dd2e87dad1e67b1876f94376cc7064aeb
Logan Linn <logan@loganlinn.com>
2025-09-30 11:09:35 -0700

    ghostty: remove duplicate keybind.config include

--- nix/home/ghostty.nix
+++ nix/home/ghostty.nix
@@ -14,7 +14,6 @@
       config-file = [
         "${config.my.flakeDirectory}/config/ghostty/options.config"
         "${config.my.flakeDirectory}/config/ghostty/keybind.config"
-        "${config.my.flakeDirectory}/config/ghostty/keybind.config"
       ]
       ++ (lib.optional pkgs.stdenv.isLinux "${config.my.flakeDirectory}/config/ghostty/linux.config")
       ++ (lib.optional pkgs.stdenv.isDarwin "${config.my.flakeDirectory}/config/ghostty/macos.config")
