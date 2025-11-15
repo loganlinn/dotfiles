6b2bf4eaf9794b6a47f5aba5d02413d33a227640
Logan Linn <logan@loganlinn.com>
2025-11-15 14:36:42 -0800

    sync

--- nix/home/git/git-spice.zsh
+++ nix/home/git/git-spice.zsh
@@ -6,16 +6,17 @@ function gs {
       env -u GITHUB_TOKEN command gs "$@"
     fi
   else
-    local max_lines=${LINES:-$(tput lines)}
-    max_lines=$((max_lines - 5))
+    git status
+    # local max_lines=${LINES:-$(tput lines)}
+    # max_lines=$((max_lines - 5))
 
-    local status_output=$(git -c color.status=always status)
-    local status_lines=$(wc -l <<<"$status_output" | tr -d ' ')
+    # local status_output=$(git -c color.status=always status)
+    # local status_lines=$(wc -l <<<"$status_output" | tr -d ' ')
 
-    echo -e "$status_output"
+    # echo -e "$status_output"
 
-    if [[ $status_lines -gt $max_lines ]]; then
-      bat <<<"$status_output"
-    fi
+    # if [[ $status_lines -gt $max_lines ]]; then
+    #   bat <<<"$status_output"
+    # fi
   fi
 }
