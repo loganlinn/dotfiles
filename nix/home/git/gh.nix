41993f5b9f52aa2f54f3fd33d7c2215a08d0ac29
Logan Linn <logan@loganlinn.com>
2026-03-04 20:09:37 -0800

    sync

--- nix/home/git/gh.nix
+++ nix/home/git/gh.nix
@@ -15,7 +15,7 @@ with lib.my;
     gitCredentialHelper.enable = true;
     settings = {
       aliases = {
-        checkout-release = ''!tag=$(gh view "$@" --json tagName --jq '.tagName') && git checkout "$tag"'';
+        release-checkout = ''!tag=$(gh release view "$@" --json tagName --jq '.tagName') && git switch "$tag"'';
         cor = ''!gh checkout release "$@"'';
         o = ''!gh browse --branch="$(git rev-parse --abbrev-ref HEAD)" .'';
         diff = "pr diff";
