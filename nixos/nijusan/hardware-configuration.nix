58b0aa6db929e213c68d75d5ac635b0509404ca8
Logan Linn <logan@loganlinn.com>
2026-03-29 14:41:47 -0700

    replace hardware-configuration.nix

--- nixos/nijusan/hardware-configuration.nix
+++ nixos/nijusan/hardware-configuration.nix
@@ -1,12 +1,9 @@
 {
   inputs,
   modulesPath,
-  lib,
   ...
-}: {
-  # Things to try
-  # - https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/services/hardware/undervolt.nix
-  # - https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/services/hardware/fancontrol.nix
+}:
+{
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
     inputs.nixos-hardware.outputs.nixosModules.common-cpu-intel
@@ -25,45 +22,34 @@
     "sd_mod"
   ];
 
-  boot.initrd.kernelModules = ["i915"];
+  boot.initrd.kernelModules = [ "i915" ];
+
+  boot.kernelModules = [ "kvm-intel" ];
 
   fileSystems = {
     "/" = {
-      device = "none";
-      fsType = "tmpfs";
+      device = "/dev/disk/by-uuid/145be6c5-8187-49cf-949f-982641b12de1";
+      fsType = "ext4";
     };
     "/boot" = {
-      device = "/dev/disk/by-uuid/C7EA-9458";
+      device = "/dev/disk/by-uuid/AE26-4F32";
       fsType = "vfat";
+      options = [
+        "fmask=0077"
+        "dmask=0077"
+      ];
     };
     "/nix" = {
-      device = "/dev/disk/by-uuid/b5ce2531-a3ee-476e-930e-83b6ecef2609";
-      fsType = "btrfs";
-      options = ["subvol=nix"];
-    };
-    "/etc" = {
-      device = "/dev/disk/by-uuid/b5ce2531-a3ee-476e-930e-83b6ecef2609";
-      fsType = "btrfs";
-      options = ["subvol=etc"];
-    };
-    "/var/log" = {
-      device = "/dev/disk/by-uuid/b5ce2531-a3ee-476e-930e-83b6ecef2609";
-      fsType = "btrfs";
-      options = ["subvol=log"];
-    };
-    "/root" = {
-      device = "/dev/disk/by-uuid/b5ce2531-a3ee-476e-930e-83b6ecef2609";
-      fsType = "btrfs";
-      options = ["subvol=root"];
+      device = "/dev/disk/by-uuid/bd669fb1-0d2b-46b2-97af-995f08f8c9f9";
+      fsType = "ext4";
     };
     "/home" = {
-      device = "/dev/disk/by-uuid/b5ce2531-a3ee-476e-930e-83b6ecef2609";
-      fsType = "btrfs";
-      options = ["subvol=home"];
+      device = "/dev/disk/by-uuid/36f52bc0-725a-4b61-938d-1149fdfd7fc1";
+      fsType = "ext4";
     };
   };
 
-  swapDevices = [{device = "/dev/disk/by-uuid/29401b32-91cd-4ab7-b7a4-58ab9a607f59";}];
+  swapDevices = [ ];
 
   nixpkgs.hostPlatform = "x86_64-linux";
 }
