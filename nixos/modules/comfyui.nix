2f24fe8193a7b730c1ab6a59892d376ade2353de
Logan Linn <logan@loganlinn.com>
2026-04-05 18:29:53 -0700

    comfyui: cuda, please

--- nixos/modules/comfyui.nix
+++ nixos/modules/comfyui.nix
@@ -1,11 +1,15 @@
 {
   inputs,
+  lib,
   ...
-}: {
+}:
+{
   imports = [ inputs.comfyui-nix.nixosModules.default ];
 
   services.comfyui = {
     enable = true;
     port = 8188;
+    gpuSupport = lib.mkDefault "cuda";
+    enableManager = true;
   };
 }
