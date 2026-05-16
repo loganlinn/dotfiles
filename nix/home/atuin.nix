f9edd7f83337e4527de9f59847d25854e6ba4944
Logan Linn <logan@loganlinn.com>
2026-05-16 15:18:49 -0700

    atuin-restart

--- nix/home/atuin.nix
+++ nix/home/atuin.nix
@@ -3,8 +3,35 @@
   pkgs,
   lib,
   ...
-}:
-{
+}: {
+  home.packages = [
+    (pkgs.writeShellApplication {
+      name = "atuin-restart";
+      runtimeInputs = with pkgs;
+        [coreutils]
+        ++ lib.optional stdenv.isLinux systemd;
+      text = ''
+        socket_path="''${XDG_DATA_HOME:-$HOME/.local/share}/atuin/daemon.sock"
+        pid_file="''${XDG_DATA_HOME:-$HOME/.local/share}/atuin/atuin-daemon.pid"
+
+        rm -f "$socket_path" "$pid_file"
+
+        case "$(uname -s)" in
+          Darwin)
+            launchctl kickstart -k "gui/$(id -u)/org.nix-community.home.atuin-daemon"
+            ;;
+          Linux)
+            systemctl --user restart atuin-daemon
+            ;;
+          *)
+            echo >&2 "atuin-restart: unsupported OS: $(uname -s)"
+            exit 1
+            ;;
+        esac
+      '';
+    })
+  ];
+
   programs.atuin = {
     daemon.enable = lib.mkDefault config.programs.atuin.enable;
     settings = {
